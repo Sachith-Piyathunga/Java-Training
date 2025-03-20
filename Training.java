de3b6118bfc0e60d848143294f2ba44e151564ee
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-03-20 18:39:44 +0530

    Create a good java Identifier

---
+++ Training.java
@@ -0,0 +1,10 @@
+// Java Identifiers
+public class Training {
+    public static void main(String[] args) {
+
+        int minutesPerhour = 60; //"minutesPerhour" good java Identifier for variable
+
+        System.out.println(minutesPerhour);
+
+    }
+}
