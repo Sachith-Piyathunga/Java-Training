70706ee56a6eb5540fb5a0277d4b9ef06c55b0eb
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-03-20 19:26:56 +0530

    Adding new things

--- Training.java
+++ Training.java
@@ -1,12 +1,23 @@
 // Java Identifiers
 public class Training {
     public static void main(String[] args) {
+        String studentName = "Sachintha";
+        int studentId = 20221948;
+        int studentAge = 25;
+        float studentMarks =75.32f;
+        char studentgrade = 'B';
 
-        int minutesPerhour = 60; //"minutesPerhour" good java Identifier for variable
-        String check = "commits are working properly";
-
-        System.out.println(minutesPerhour);
-        System.out.println(check);
+        System.out.println(studentName);
+        System.out.println(studentId);
+        System.out.println(studentAge);
+        System.out.println(studentMarks);
+        System.out.println(studentgrade);
 
     }
 }
+
+/*int minutesPerhour = 60; //"minutesPerhour" good java Identifier for variable
+        String check = "commits are working properly";
+
+        System.out.println(minutesPerhour);
+        System.out.println(check); */
