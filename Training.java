1e69d4fff70d0a787dbe2a7febad7117d8dbcfd0
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-03-20 19:17:35 +0530

    check commits are working in VS code

--- Training.java
+++ Training.java
@@ -3,8 +3,10 @@ public class Training {
     public static void main(String[] args) {
 
         int minutesPerhour = 60; //"minutesPerhour" good java Identifier for variable
+        String check = "commits are working properly";
 
         System.out.println(minutesPerhour);
+        System.out.println(check);
 
     }
 }
