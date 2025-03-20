02a2e62838c6e8bd1c37b9d4846e855b2bd688b7
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-03-20 19:33:37 +0530

    Calculate the Area of a Rectangle

--- Training.java
+++ Training.java
@@ -1,6 +1,29 @@
 // Java Identifiers
-public class Training {
-    public static void main(String[] args) {
+public class Training{
+    public static void main(String[] args){
+        // Create integer variables
+        int length = 4;
+        int width = 6;
+        int area;
+
+        // Calculate the area of a rectangle
+        area  = length *width;
+
+        // Print variables
+        System.out.println("length is: " + length);
+        System.out.println("width is: " + width);
+        System.out.println("Area of the rectangle is: " + area);
+
+
+    }
+}
+
+/*
+        int minutesPerhour = 60; //"minutesPerhour" good java Identifier for variable
+        String check = "commits are working properly";
+
+        System.out.println(minutesPerhour);
+        System.out.println(check); 
         String studentName = "Sachintha";
         int studentId = 20221948;
         int studentAge = 25;
@@ -13,11 +36,4 @@ public class Training {
         System.out.println(studentMarks);
         System.out.println(studentgrade);
 
-    }
-}
-
-/*int minutesPerhour = 60; //"minutesPerhour" good java Identifier for variable
-        String check = "commits are working properly";
-
-        System.out.println(minutesPerhour);
-        System.out.println(check); */
+        */
