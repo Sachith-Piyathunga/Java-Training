051400025106764a5daecefc86ea63ca6b446b8e
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-19 15:49:52 +0530

    feat: Rearrange the variables in correct order

--- Training.java
+++ Training.java
@@ -4,12 +4,11 @@
 public class Training {
     public static void main(String[] args){
     
-    tring year = 2001;
-    double title = "Shrek";
-    int genre = 'C';
-    boolean runtime = 1.58;
-    char isPG = true;
-
+    int year = 2001;
+    String title = "Shrek";
+    char genre = 'C';
+    double runtime = 1.58;
+    boolean isPG = true;
 
 }
 
