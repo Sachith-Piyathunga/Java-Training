58eb3911d6f0b29e1b1308d1bda88ad9c6c3e53c
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-19 16:22:01 +0530

    feat: Check and fix all veriable names

--- Training.java
+++ Training.java
@@ -4,9 +4,9 @@
 public class Training {
     public static void main(String[] args){
     
-    tring 1stName = "Samira";
+    String firstName = "Samira";
     String blah = "Smith";
-    String .com = "samira@google.com";
+    String com = "samira@google.com";
     int salaryexpectation = 100000;
     int year_of_birth = 1955;
     
