2047f01ab746f32712f721069659934db393a291
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-19 16:19:29 +0530

    feat: Test bad variable name

--- Training.java
+++ Training.java
@@ -4,12 +4,13 @@
 public class Training {
     public static void main(String[] args){
     
-    int year = 2001;
-    String title = "Shrek";
-    char genre = 'C';
-    double runtime = 1.58;
-    boolean isPG = true;
-
+    tring 1stName = "Samira";
+    String blah = "Smith";
+    String .com = "samira@google.com";
+    int salaryexpectation = 100000;
+    int year_of_birth = 1955;
+    
+    System.out.println("The program runs!");
 }
 
 }
