cf02fe483958b300bcedff9e738bba21fd0e9a27
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-21 12:24:58 +0530

    feat: Create new variables

--- Training.java
+++ Training.java
@@ -4,30 +4,12 @@
 public class Training {
     public static void main(String[] args){
     
-      String name1 = "Sachintha";
-      String adjective1 = "good";
-      String adjective2 = "great";
-      String adjective3 = "angry";
-      String verb1 = "sing";
-      String noun1 = "seller";
-      String noun2 = "food";
-      String noun3 = "screem";
-      String noun4 = "police officer";
-      String noun5 = "lake";
-      String noun6 = "German";
-      String name2 = "Chamod";
-      int number = 2025;
-      String place1 = "city";
+	int zebrasInZoo = 8;
+    int giraffesInZoo = 4;
+    int animalsInZoo = zebrasInZoo + giraffesInZoo;
+    int numZebrasAfterTrade = zebrasInZoo - 2;
 
-
-//The template for the story
-      String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+
-      adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+
-      verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3
-      +". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+
-      name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
     
-      System.out.println(story);
 }
 
 }
