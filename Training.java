b924aaa2c488ef589241f3824f9ab5b1ad12d2b6
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-21 18:20:53 +0530

    feat: Use string concatination

--- Training.java
+++ Training.java
@@ -4,12 +4,13 @@
 public class Training {
     public static void main(String[] args){
     
-      String line1 = "Nah nah nah nah nah nah nah nah nah yeah";
-      String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
-      String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
-      
-      System.out.println(line1.equals(line2));
-      System.out.println(line2.equals(line3));
+      // String concatination
+        int animals = 12;
+         String species = "zebra";
+         String zooDescription = "Our zoo has " + animals + " " + species + "s!";
+
+        System.out.println(zooDescription);
+
 
     }
 
