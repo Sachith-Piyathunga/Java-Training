01f9e9c5b31f1ae30bb393ed4b614049cdba9778
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-21 18:11:39 +0530

    feat: Check ines are same using best practices

--- Training.java
+++ Training.java
@@ -4,15 +4,12 @@
 public class Training {
     public static void main(String[] args){
     
-    String person1 = "Paul";
-    String person2 = "John";
-    String person3 = "Paul";
-
-    System.out.println(person1.equals(person2));
-    // Prints false, since "Paul" is not "John"
-
-    System.out.println(person1.equals(person3));
-    // Prints true, since "Paul" is "Paul"
+      String line1 = "Nah nah nah nah nah nah nah nah nah yeah";
+      String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+      String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+      
+      System.out.println(line1.equals(line2));
+      System.out.println(line2.equals(line3));
 
     }
 
