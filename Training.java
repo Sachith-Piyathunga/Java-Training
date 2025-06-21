6e7e3826aeb7dd860db94ea4ea8c9109b13ca5cd
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-21 18:34:29 +0530

    feat: Try all the concepts learn so far

--- Training.java
+++ Training.java
@@ -4,12 +4,15 @@
 public class Training {
     public static void main(String[] args){
     
-        // Final keyword
-        final double pi = 3.14;
-        double pi = 5.24;
+     double balance = 1000.75;
+      double amountToWithdraw = 250;
+      double updatedBalance = balance - amountToWithdraw;
+      double amountForEachFriend = updatedBalance / 3;
+      boolean canPurchaseTicket = amountForEachFriend > 250;
 
+      System.out.println(canPurchaseTicket);
+      System.out.println("I gave each friend " + amountForEachFriend);
 
-        System.out.println(pi);
 
 
     }
