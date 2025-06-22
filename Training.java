f173ea5eb3b414de42d7dc03f67a6cc824537939
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-22 06:42:38 +0530

    feat: Create a constructor

--- Training.java
+++ Training.java
@@ -4,15 +4,9 @@
 public class Training {
     public static void main(String[] args){
     
-     double balance = 1000.75;
-      double amountToWithdraw = 250;
-      double updatedBalance = balance - amountToWithdraw;
-      double amountForEachFriend = updatedBalance / 3;
-      boolean canPurchaseTicket = amountForEachFriend > 250;
-
-      System.out.println(canPurchaseTicket);
-      System.out.println("I gave each friend " + amountForEachFriend);
-
+    public Store(){
+        
+        System.out.println("I am inside the Store constructor");
 
 
     }
