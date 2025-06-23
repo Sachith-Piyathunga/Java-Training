c0b5746e3c518146fba34f2aef9f72bc40072bf7
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-23 11:38:57 +0530

    feat: Add another instance

--- Training.java
+++ Training.java
@@ -2,11 +2,10 @@
 
 // Java Identifiers
 public class Training {
-    public static void main(String[] args){
-    
-    Store newStore;
-    newStore = new Store();
-}
+  public static void main(String[] args) {    
+    Store cookieShop = new Store("cookies", 12, 3.75);
+
+  }
 
 }
 
