8686fb9f5121df895158f823f92310e3f6cd222d
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-25 19:03:19 +0530

    feat: Try if else condition

--- Order.java
+++ Order.java
@@ -2,11 +2,14 @@ public class Order {
   
   public static void main(String[] args) {
     
-    double itemCost = 30.99;
+    boolean isFilled = false;
     
-    // Write an if-then statement:
-    if(itemCost > 24){
-      System.out.println("High value item!");
+    // Write an if-then-else statement:
+    if(isFilled){
+      System.out.println("Shipping.");
+
+    }else{
+      System.out.println("Order not ready.");
 
     }
     
