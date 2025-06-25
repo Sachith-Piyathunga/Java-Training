2871ad3a24c7f4c30e58ad6ee610fc2c00970ea9
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-25 12:09:56 +0530

    fix: Fix erros in the man method class

--- Training.java
+++ Training.java
@@ -1,10 +1,13 @@
 //import java.util.Scanner;
 public class Training {
 
+
   public static void main(String[] args) {
-    Store lemonadeStand = new Store("Lemonade");
-    lemonadeStand.greetCustomer("Sachintha");
+    Store lemonadeStand = new Store("Lemonade", 3.75);
+    lemonadeStand.increasePrice(1.50);
+    System.out.println(lemonadeStand.price);
   }
+
 
 
     
