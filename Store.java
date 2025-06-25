57c32d97aba9c80f118e752585702f04ac06dfa7
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-25 12:43:17 +0530

    feat: Create a toString method

--- Store.java
+++ Store.java
@@ -1,27 +1,16 @@
 public class Store {
   // instance fields
-  String productType;
-  double price;
-  double tax = 0.08;
+  public String productType;
+  public double price;
     
   // constructor method
   public Store(String product, double initialPrice) {
     productType = product;
     price = initialPrice;
   }
-    
-  // increase price method
-  public void increasePrice(double priceToAdd){
-    double newPrice = price + priceToAdd;
-    price = newPrice;
-  }
-    
-  // get price with tax method
-  public double getPriceWithTax(){
-    double totalPrice = price + price * tax;
-    return totalPrice;
 
+  public String toString(){
+    
   }
   
-
 }
