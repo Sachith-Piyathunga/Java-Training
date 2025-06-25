f782966a2413e39217fca14fd9cf137d4316783b
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-25 11:02:31 +0530

    feat: Add new statements

--- Store.java
+++ Store.java
@@ -5,12 +5,13 @@ public class Store {
   // constructor method
   public Store(String product) {
     productType = product;
+    System.out.println("Selling " + product + "!" );
   }
     
   // Add advertise method below
   public void advertise(){
-    
+    System.out.println("Come spend some money!" );
+
   }
     
-
 }
