95d37004665c09a5bfb5d27406dea57e19e64c68
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-26 20:27:47 +0530

    feat: Check length of the array

--- Newsfeed.java
+++ Newsfeed.java
@@ -1,16 +1,18 @@
 public class Newsfeed {
   String[] topics = {"Opinion", "Tech", "Science", "Health"};
   int[] views = {0, 0, 0, 0};
-  String[] favoriteArticles;
   
   public Newsfeed(){
-    // Initialize favoriteArticles here:
-    favoriteArticles = new String[10];
+
+  }
+    
+  public String[] getTopics(){
+    return topics;
+  }
+  
+  public int getNumTopics(){
+    return topics.length;
     
   }
   
-  public void setFavoriteArticle(int favoriteIndex, String newArticle){
-    // Add newArticle to favoriteArticles:
-    favoriteArticles[favoriteIndex] = newArticle;
-  }
 }
