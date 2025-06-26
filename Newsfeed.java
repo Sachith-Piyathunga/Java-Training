a931b4f4e96fb5e4b092a0ff7f67b3c42e2fc1e8
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-26 20:13:49 +0530

    feat: Add new array

--- Newsfeed.java
+++ Newsfeed.java
@@ -1,23 +1,16 @@
 public class Newsfeed {
   String[] topics = {"Opinion", "Tech", "Science", "Health"};
-  public int[] views = {0, 0, 0, 0};
+  int[] views = {0, 0, 0, 0};
+  String[] favoriteArticles;
   
   public Newsfeed(){
-
-  }
+    // Initialize favoriteArticles here:
+    String[] favoriteArticles = new String[10];
     
-  public String[] getTopics(){
-    return topics;
   }
   
-  // Create getFirstTopic() below
-  public String getFirstTopic(){
-    return topics[0];
-  }
-
-  
-  public void viewTopic(int topicIndex){
-    views[topicIndex]++;
+  public void setFavoriteArticle(int favoriteIndex, String newArticle){
+    // Add newArticle to favoriteArticles:
     
   }
 }
