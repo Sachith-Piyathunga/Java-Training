4fbc9f437051bff82d96a27df555211c47a56b80
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-26 21:22:02 +0530

    feat: Set values

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
       String[] students = {"Sade", "Alexus", "Sam", "Koma"};
+      int [] mathScores = new int[4]; 
 
       
   }
