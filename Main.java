2b753996e0d19aa5381478fd616d26badd0146cf
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-26 21:43:58 +0530

    feat: Check code is work

--- Main.java
+++ Main.java
@@ -1,18 +1,16 @@
 import java.util.Arrays;
 
-public class Main {
-    public static void main(String[] args) {
-      String[] students = {"Sade", "Alexus", "Sam", "Koma"};
-      int [] mathScores = new int[4]; 
-      mathScores [0] = 64;
-      mathScores [2] = 76;
-      mathScores [1] = 57;
-      mathScores [3] = 98;
-
-      for(int i = 0; i < students.length; i++){
-      System.out.println(students[i] + ": " + mathScores[i]);
-    }
-
-      
+class Shopping {
+  
+  public static void main(String[] args) {
+    
+    String[] groceryItems = {"steak", "milk", "jelly beans"};
+    double[] prices = {25.00, 2.95, 2.50};
+    
+    // Adding ham to the groceries
+    groceryItems[3] = "ham";
+    prices[3] = 4.99;
+    
   }
+  
 }
