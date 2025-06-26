2e79f512ce1cae5e8847919fa07d7279ccc8c823
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-26 20:00:30 +0530

    feat: Create new method

--- Newsfeed.java
+++ Newsfeed.java
@@ -1,11 +1,22 @@
 public class Newsfeed {
+  String[] topics = {"Opinion", "Tech", "Science", "Health"};
+  public int[] views = {0, 0, 0, 0};
+  
   public Newsfeed(){
-      
-  }
-  // Create getTopics() below:
-  public String[] getTopics(){  
-    String[] topics = {"Opinion", "Tech", "Science", "Health"};
-    return topics;
+
   }
     
+  public String[] getTopics(){
+    return topics;
+  }
+  
+  // Create getFirstTopic() below
+  public String getFirstTopic(){
+    return topics[0];
+  }
+
+  
+  public void viewTopic(int topicIndex){
+    
+  }
 }
