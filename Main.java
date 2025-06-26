8c5ed33e1d222c7781292383f951c6827d318279
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-26 22:31:04 +0530

    feat: Add more todo lists

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
-// import the ArrayList package here:
 import java.util.ArrayList;
 
-
 class ToDos {
-  
+    
   public static void main(String[] args) {
     
-    // Create toDoList below:
     ArrayList<String> toDoList = new ArrayList<String>();
+    String toDo1 = "Water plants";
+    // Add more to-dos here:
+    String toDo2 = "Sachintha";
+    String toDo3 = "Chamod";   
+    // Add to-dos to toDoList
     
+    
+    System.out.println(toDoList);
+      
     
   }
   
