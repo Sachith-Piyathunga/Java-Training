1fa1202ab85694222a6179c7798afb05861777ad
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-26 21:27:50 +0530

    feat: Add values

--- Main.java
+++ Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
       String[] students = {"Sade", "Alexus", "Sam", "Koma"};
       int [] mathScores = new int[4]; 
-
+      mathScores [0] = 64;
+      mathScores [2] = 76;
+      mathScores [1] = 57;
+      mathScores [3] = 98;
       
   }
 }
