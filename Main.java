ed3dcd4ef27f8abadd39de65d80dc5122e2ddef6
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-27 12:12:17 +0530

    feat: Create a new for loop

--- Main.java
+++ Main.java
@@ -1,12 +1,23 @@
-class Coffee {
+import java.util.ArrayList;
+
+class CalculateTotal {
   
   public static void main(String[] args) {
     
-    //int cupsOfCoffee = 1;
+    ArrayList<Double> expenses = new ArrayList<Double>();
+    expenses.add(74.46);
+    expenses.add(63.99);
+    expenses.add(10.57);
+    expenses.add(81.37);
     
-    for(int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++){
-      System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+    double total = 0;
+    
+    // Iterate over expenses
+    for(int i = 0; i < expenses.size(); i++){
+      
     }
+    
+    System.out.println(total);
     
   }
   
