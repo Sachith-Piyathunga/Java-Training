469675bbdbd134d155d44b68469a2997e1b0489a
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-27 18:31:52 +0530

    style: Change some values

--- Person.java
+++ Person.java
@@ -24,8 +24,9 @@ public class Person{
   public void hasBirthday(){
     //Complete this method
     this.setAge(this.age + 1);
-    this.setWisdom(this.wisdom + 10);
+    this.setWisdom(this.wisdom + 5);
     this.setFitness(this.fitness - 3);
+    
     
   }
 }
