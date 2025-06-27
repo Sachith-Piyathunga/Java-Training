fd810657e522bab7f6fbe5fae47396274e1f9f8c
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-27 12:42:22 +0530

    feat: Add for each loop

--- Main.java
+++ Main.java
@@ -1,13 +1,25 @@
-class Numbers {
+import java.util.ArrayList;
+
+class MostExpensive {
+  
   public static void main(String[] args) {
-    for (int i = 0; i < 100; i++) {
-      // Add your code below
-      if(i % 5 != 0){
-        continue;
-      }
-      System.out.println(i);
-      
-    }
-  }
-}
     
+    ArrayList<Double> expenses = new ArrayList<Double>();
+    expenses.add(74.46);
+    expenses.add(63.99);
+    expenses.add(10.57);
+    expenses.add(81.37);
+    
+    double mostExpensive = 0;
+    
+    // Iterate over expenses
+    for(double expense : expenses){
+
+    }
+    
+    
+    System.out.println(mostExpensive);
+    
+  }
+  
+}
