ca1034c6854b16eec28a72611bb9c19ecb6a5feb
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-27 13:18:34 +0530

    feat: Add variable names

---
+++ Initials.java
@@ -0,0 +1,14 @@
+public class Initials {
+  
+	public static void main(String[] args) {
+    
+    // Add a first name and a last name:
+    String firstName = "Sachintha";  
+    String lastName = "Piyathunga";
+    
+    // What are the initials?
+    
+    
+  }
+  
+}
