8f4389be97b83113b6212709754b9d3453b875ea
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-27 12:05:38 +0530

    feat: Cnvert while loop into for loop

--- Main.java
+++ Main.java
@@ -2,13 +2,10 @@ class Coffee {
   
   public static void main(String[] args) {
     
-    // initialize cupsOfCoffee
-    int cupsOfCoffee = 1;
+    //int cupsOfCoffee = 1;
     
-    // add while loop with counter
-    while(cupsOfCoffee <= 100){
+    for(int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++){
       System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
-      cupsOfCoffee ++;
     }
     
   }
