7419e7960e25c16ac8d89a00a541b185a534e600
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-27 12:18:45 +0530

    feat: Get total

--- Main.java
+++ Main.java
@@ -15,9 +15,10 @@ class CalculateTotal {
     // Iterate over expenses
     for(int i = 0; i < expenses.size(); i++){
       System.out.println(expenses.get(i));
+      total += expenses.get(i);
     }
     
-    System.out.println(total);
+    System.out.println("Total : " + total);
     
   }
   
