fe75230796ca20794fdf31d8f950bd2628b66b05
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-28 18:46:28 +0530

    feat: Create an instance in the main class

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main{
   public static void main(String[] args) {
+    Spaghetti spaghettiPomodoro = new Spaghetti();
+
+    System.out.println(spaghettiPomodoro.texture);
      
   }
 }
