c9bcd216ae2660a3301653ba6afeab11621d0ebb
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-28 19:07:22 +0530

    feat: Change main class

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main{
-  
   public static void main(String[] args) {
-    Pho phoChay = new Pho();
-    System.out.println(phoChay.shape);
-      
+    Ramen yasaiRamen = new Ramen();
+    //System.out.println(yasaiRamen.ingredients);
+    System.out.println(yasaiRamen.isTasty());  
   }
 }
