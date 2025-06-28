7d42473d9a13f4b4e2559f86ceb2052c312e7568
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-28 19:22:25 +0530

    feat: Change code

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main{
   public static void main(String[] args) {
-    Ramen yasaiRamen = new Ramen();
-    //System.out.println(yasaiRamen.ingredients);
-    System.out.println(yasaiRamen.isTasty());  
+    Spaetzle kaesespaetzle = new Spaetzle();
+    kaesespaetzle.cook();
   }
 }
