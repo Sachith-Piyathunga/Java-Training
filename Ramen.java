898738622904a8b376b0cb6df52c7ffd999cdb19
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-28 21:37:26 +0530

    feat: Change the ramen class

--- Ramen.java
+++ Ramen.java
@@ -1,7 +1,11 @@
-class Dinner {
-  public void makeNoodles(Noodle noodle, String sauce) {
-    noodle.cook();
-    System.out.println("Mixing " + noodle.texture + " noodles made from " + noodle.ingredients + " with " + sauce + ".");
-    System.out.println("Dinner is served!");
-  }  
+class Ramen extends Noodle {
+
+  Ramen() {
+    super(30.0, 0.3, "flat", "wheat flour");
+  }
+  
+  @Override
+  public String getCookPrep() {
+    return "Boil ramen for 5 minutes in broth, then add meat, mushrooms, egg, and vegetables.";
+  }
 }
