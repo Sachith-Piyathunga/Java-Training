ebf302b4c8061d6d29ce9950c55698c4c0c334e6
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-28 21:22:53 +0530

    feat: Add instance method

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
   public static void main(String[] args) {
     Dinner noodlesDinner = new Dinner();
     // Add your code here: 
+    Noodle biangBiang = new BiangBiang();
   }
 }
