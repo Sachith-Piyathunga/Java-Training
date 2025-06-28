5cdb5f3608621233e5805ac11ad47e8cee6db68a
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-28 18:47:13 +0530

    feat: Change main clas

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
 public class Main{
   public static void main(String[] args) {
-    Spaghetti spaghettiPomodoro = new Spaghetti();
-
-    System.out.println(spaghettiPomodoro.texture);
-     
+      
   }
 }
