aea71d834eccd9c486988d4073a3004630e29b8b
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-28 18:41:31 +0530

    feat: Create a new class

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,27 +1,11 @@
-public class CheckingAccount{
-  private String name;
-  private int balance;
-  private String id;
-  private double interestRate;
+class Noodle {
+  double lengthInCentimeters;
+  String shape;
+  String texture = "brittle";
   
-  public CheckingAccount(String inputName, int inputBalance, String inputId){
-    this.name = inputName;
-    this.balance = inputBalance;
-    this.id = inputId;
-    this.interestRate = 0.02;
+  public void cook() {
+    
+    this.texture = "cooked";
+    
   }
-  
-  public void getAccountInformation(){
-    System.out.println("Money in account: " + this.getBalance());
-    System.out.println("Next Month's Interest: " + this.calculateNextMonthInterest());
-  }
-
-  private double calculateNextMonthInterest (){
-    return this.balance * this.interestRate;
-  }
-  
-  private int getBalance(){
-    return this.balance;
-  }
-  
 }
