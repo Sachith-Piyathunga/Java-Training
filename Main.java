b81225bebd2a5cd88ab7798373a0d41009a3dc59
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-29 12:50:17 +0530

    feat: Create a new array and assign a valuews

--- Main.java
+++ Main.java
@@ -1,12 +1,11 @@
 public class Main {
 	public static void main(String[] args) {
-		// Declare a 2d array of float values called floatTwoD
-    float[][] floatTwoD;
-		floatTwoD = new float[4][10];
-		
-    char[][] ticTacToe = {{'X', 'O', 'O'}, {'O', 'X', ' '}, {'X', ' ', 'X'}};
-		
-    ticTacToe = new char[][] {{'O', 'X', 'X'}, {'X', 'O', ' '}, {'O', ' ', 'O'}};
-    
-    }
+		// Using the provided 2D array
+	  int[][] intMatrix = {
+				{1, 1, 1, 1, 1},
+				{2, 4, 6, 8, 0},
+				{9, 8, 7, 6, 5}
+		};
+    int retrievedInt = intMatrix[0][3];
+  }
 }
