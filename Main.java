77f882eece19ca9ae2ec0ac87cadf673eddcebcf
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-29 10:27:47 +0530

    feat: Change the main code

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-  public static void main(String[] args) {
-    int steps = 10;
-    
-    for (int i = 1; i <= steps; i++) {
-      System.out.println("Step #" + i);
-    }
-  }
-}
