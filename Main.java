2a26326826417179b59f15a7ec3a53388d149284
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-29 13:12:35 +0530

    feat: 2D array traversing

--- Main.java
+++ Main.java
@@ -1,12 +1,22 @@
 public class Main {
 	public static void main(String[] args) {
-		int tableSize = 10;
-    for(int i = 1; i <= 10; i++){
-      for(int j = 1; j <= 10; j++){
-        System.out.print(i * j);
-        System.out.print(" ");
-      }
-      System.out.println();
-    }
+		//Given the provided 2d array
+		int[][] intMatrix = {
+				{ 4,  6,  8, 10, 12, 14, 16},
+				{18, 20, 22, 24, 26, 28, 30},
+				{32, 34, 36, 38, 40, 42, 44},
+				{46, 48, 50, 52, 54, 56, 58},
+				{60, 62, 64, 66, 68, 70, 79}
+		};
+		
+		
+		int sum = 0;
+		for(int i=0; i<-1; i++) {
+			for(int j = 0; j < -1; j++) {
+				// Add a line to calculate sum of all elements
+				
+			}
+		}
+		System.out.println(sum);
 	}
 }
