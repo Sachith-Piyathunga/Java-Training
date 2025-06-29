3cd47081fb06014471d6b17d2a6d88a85c5850bb
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-29 10:27:09 +0530

    feat: Change the main code loop

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main{
   public static void main(String[] args) {
     int steps = 10;
     
-    for (int i = 0; i <= steps; i++) {
+    for (int i = 1; i <= steps; i++) {
       System.out.println("Step #" + i);
     }
   }
