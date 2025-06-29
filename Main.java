8cb598ac12be13587ddf4d49979f35ef50aee0e3
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-29 13:05:17 +0530

    feat: Nested loops for array

--- Main.java
+++ Main.java
@@ -1,21 +1,12 @@
-import java.util.Arrays;
 public class Main {
 	public static void main(String[] args) {
-		// Using the provided 2D array
-	  int[][] intMatrix = {
-				{1, 1, 1, 1, 1},
-				{2, 4, 6, 8, 0},
-				{9, 8, 7, 6, 5}
-		};   
-    
-		// Declare and initialize a 2D array called subMatrix
-    int[][] subMatrix = new int[2][2];
-    subMatrix[0][0] = intMatrix[0][0] * 5;
-    subMatrix[0][1] = intMatrix[0][1] * 5;
-    subMatrix[1][0] = intMatrix[1][0] * 5;
-    subMatrix[1][1] = intMatrix[1][1] * 5;
-		
-    
-   System.out.println(Arrays.deepToString(subMatrix));
+		int tableSize = 10;
+    for(int i = 0; i < 10; i++){
+      for(int j = 10; j > 1; j--){
+        System.out.print(i * j);
+        System.out.print(" ");
+      }
+      System.out.println();
     }
+	}
 }
