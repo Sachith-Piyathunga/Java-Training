f9d890271d6786605a13b8fea11b705d0646b89d
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-29 09:43:22 +0530

    feat: Change the main code and check the error

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
-public class Main {
+public class Main{
   public static void main(String[] args) {
-    int[] numbers = {1, 2, 3, 4, 5};
-        
-    int lastNumber = numbers[4];
-        
-    System.out.println("The value of the last element is: " + lastNumber);
-  }
+   int width = 0;
+   int length = 40;
+   int ratio = length / width;
+ }
 }
