0b0537fcf142023772da08426425d7c029e26b18
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-29 12:32:13 +0530

    feat: Initialize 4 rows and 10 collumns

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
 	public static void main(String[] args) {
 		// Declare a 2d array of float values called floatTwoD      
     float [][] floatTwoD;
+    float floatTwoD = new float[4][10];
     }
 }
