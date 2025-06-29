7fdff211cef1a71c56763867188c6d4adc331859
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-06-29 13:33:01 +0530

    feat: Array review

--- Main.java
+++ Main.java
@@ -1,35 +1,33 @@
 import java.util.Arrays;
 public class Main {
-	public static void main(String[] args) {
-		int[][] imageData={
-      {100,90,255,80,70,255,60,50},
-			{255,10,5,255,10,5,255,255},
-			{255,255,255,0,255,255,255,75},
-			{255,60,30,0,30,60,255,255}
-    };
-		// Declare and initialize the 2D array newImage
-		int[][] newImage = new int[4][6];
+	public static void main(String[] args) {  
 
-		// Add a nested `for` loop and copy the data of `imagedata` to `newImage`
-    for(int i=0; i<newImage.length; i++){
-      for(int j=0; j<newImage[i].length; j++){
-        newImage[i][j] = imageData[i][j];
-      }
-    }
+		//Declare and initialize a 4x3 2D array of doubles called `scores` below
+    double[][] scores = {
+			{80.4, -1, -1},
+			{96.2, -1, -1},
+			{100.0, -1, -1},
+			{78.9, -1, -1}
+		};
 
-		System.out.println(Arrays.deepToString(newImage));
-			
-		for(int i=0; i<newImage.length; i++){
-      for(int j=0; j<newImage[i].length; j++){
-        // Add the if-else statement here
-        if(newImage[i][j]-50<0){
-          newImage[i][j] = 0;
-        }
-        else{
-          newImage[i][j]-=50;
-        }
-      }
-    }
-		System.out.println(Arrays.deepToString(newImage));
+
+  	System.out.println(Arrays.deepToString(scores));
+
+		//Manually enter the scores for the second exam below
+
+
+  	System.out.println(Arrays.deepToString(scores));
+		
+  	//Declare and initialize an empty 4x2 2D array of double values called `newScores` below
+
+
+  	//Use `for` loops to copy the scores below
+    
+
+  	System.out.println(Arrays.deepToString(newScores));
+
+  	//Iterate through the `newScores` 2D array and use `if` statement to add 2 additional points below
+    
+    System.out.println(Arrays.deepToString(newScores));
 	}
 }
