5365ff811f4f4e7e84dfd146de29c426a5363149
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-04 19:52:57 +0530

    feat: Get 2 numbers from youser

--- Main.java
+++ Main.java
@@ -6,6 +6,12 @@ public class Main {
     Scanner scr = new Scanner(System.in);
 
     System.out.println("Enter your first number: ");
+    int num1 = scr.nextInt();
+
+    System.out.println("Enter your seconed number: ");
+    int num2 = scr.nextInt();
+
+    System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
 		
 	}
 }
