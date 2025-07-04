555145817f100d350f56b43261f9717e0d704d54
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-04 19:49:03 +0530

    feat: Add scanner for get user input

--- Main.java
+++ Main.java
@@ -1,37 +1,11 @@
 //import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {  
-    /**
-     * a. -5 + 8 * 6
-     * b. (55+9) % 9
-     * c. 20 + -3*5 / 8
-     * d. 5 + 15 / 3 * 2 - 8 % 3
-     */
+    Scanner scr = new Scanner(System.in);
 
-    int a = -5;
-    int b = 8;
-    int c = 6;
-    int d = 55;
-    int e = 9;
-    int f = 20;
-    int g = 3;
-    int h = 5;
-    int i = 15;
-    int j = 2;
-    int k = 3;
-
-    int res1 = a + b * c;
-    System.out.println("Answer of a is: " + res1);
-
-    int res2 = (d + e) % e;
-    System.out.println("Answer of b is: " + res2);
-
-    int res3 = f + a * h / b;
-    System.out.println("Answer of c is: " + res3);
-
-    int res4 =  h + i / k * j - b % k;
-    System.out.println("Answer of d is: " + res4);
+    System.out.println("Enter your first number: ");
 		
 	}
 }
