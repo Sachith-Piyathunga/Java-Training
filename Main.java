218371ad8eaa6b2fa88a14212afc2772bd3bc287
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-05 00:56:58 +0530

    feat: Try basic question

--- Main.java
+++ Main.java
@@ -3,11 +3,19 @@
 
 public class Main {
 	public static void main(String[] args) {  
-    // Create an patten called java
-    System.out.println("   J     a   a     a   a    ");
-    System.out.println("   J    a a   a   a   a  a  ");
-    System.out.println("J   J  aaaaa   a a   aaaaaa ");
-    System.out.println(" j j  a     a   a   a      a");
+    /**
+     * ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+     * Expected Output
+     * 2.138888888888889
+     */
+    double a = 25.5;
+    double b = 3.5;
+    double c = 40.5;
+    double e = 4.5;
+
+    double result = ((a * b - b * b) / (c - e));
+
+    System.out.println("The final result is: " + result);
     
 	}
 }
