c5a794cc1799be5c3d92749f5081dfa295a9af0c
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-04 19:21:54 +0530

    feat: Get devition of the two numbers

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@
 
 public class Main {
 	public static void main(String[] args) {  
-    int num1 = 50;
-    int num2 = 3;
+    double num1 = 50;
+    double num2 = 3;
 
-    int sum = num1 + num2;
+    double sum = num1 / num2;
 
     System.out.println("Sum of two numbers are: " + sum);
 		
