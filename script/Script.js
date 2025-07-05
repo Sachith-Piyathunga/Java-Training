fd1b15c111ceffae79787fd309baca50efd43d4d
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-06 02:57:45 +0530

    feat: Change parameters

--- script/Script.js
+++ script/Script.js
@@ -1,5 +1,7 @@
-function sayThanks(name) {
-  console.log('Thank you for your purchase, ' + name + '! We appreciate your business.');
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
 }
 
-sayThanks('Cole');
+makeShoppingList();
