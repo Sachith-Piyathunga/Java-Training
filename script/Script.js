728e5a316afbdf96a7198155732ddb9dea04253e
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-05 18:53:43 +0530

    feat: Check and add else if stetments

--- script/Script.js
+++ script/Script.js
@@ -4,6 +4,10 @@ if (season === 'spring') {
   console.log('It\'s spring! The trees are budding!');
 } else if (season === 'winter') {
   console.log('It\'s winter! Everything is covered in snow.');
-}else {
+} else if (season === 'fall') {
+  console.log('It\'s fall! Leaves are falling!');
+} else if (season === 'summer') {
+  console.log('It\'s sunny and warm because it\'s summer!');
+} else {
   console.log('Invalid season.');
 }
