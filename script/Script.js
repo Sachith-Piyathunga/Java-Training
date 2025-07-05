4dad7b04d30849ff60e1aaa75985205625b9db31
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-05 15:48:37 +0530

    feat: Check typrof oparetor

--- script/Script.js
+++ script/Script.js
@@ -1,6 +1,4 @@
-// String Interpolation
+// typeof operator
 
-const myName = 'Sachintah';
-const myCity = 'Athans';
-
-console.log(`My name is ${myName}. My favorite city is  ${myCity}`); // to the string interpolation we use this symbols --> $ and `
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
