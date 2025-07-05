72d4623ef7df1867c34c3a5177c154fd52348989
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-06 03:35:24 +0530

    feat: Change full code into one single line

--- script/Script.js
+++ script/Script.js
@@ -1,7 +1,1 @@
-const plantNeedsWater = (day) => { // const plantNeedsWater = function (day)
-  if (day === 'Wednesday') { // Using a => fat arrow , we can skip the keyword call function
-    return true;
-  } else {
-    return false;
-  }
-};
+const plantNeedsWater = day => day === 'Wednesday' ? true : false;
