ec1b52fae1822ae4d0ffbb62a261ad13bf155012
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-05 19:12:41 +0530

    feat: Set more csaes

--- script/Script.js
+++ script/Script.js
@@ -4,5 +4,10 @@ switch(athleteFinalPosition) {
   case 'first place':
     console.log('You get the gold medal!');
     break;
-  
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
 }
