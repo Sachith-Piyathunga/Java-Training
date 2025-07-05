f7ecdeeaea52d385d12cdb67f72c11bc78069b84
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-06 03:01:31 +0530

    feat: Check return stetment

--- script/Script.js
+++ script/Script.js
@@ -1,7 +1,5 @@
-function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
-  console.log(`Remember to buy ${item1}`);
-  console.log(`Remember to buy ${item2}`);
-  console.log(`Remember to buy ${item3}`);
+function rectangleArea(width, height) {
+  let area = width * height;
+  return area;
 }
-
-makeShoppingList();
+console.log(rectangleArea(5, 7)) // Prints undefined
