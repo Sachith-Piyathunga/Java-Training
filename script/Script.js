df8c2716075f249f3e904161ef92a2ad96e97925
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-05 18:34:37 +0530

    feat: Change the coe

--- script/Script.js
+++ script/Script.js
@@ -1,6 +1,23 @@
-let tool = 'marker';
+let isLocked = false;
 
-// Use short circuit evaluation to assign  writingUtensil variable below:
-let writingUtensil = tool || 'pen';
+if (isLocked) {
+  console.log('You will need a key to open the door.');
+} else {
+  console.log('You will not need a key to open the door.');
+}
 
-console.log(`The ${writingUtensil} is mightier than the sword.`);
+let isCorrect = true;
+
+if (isCorrect) {
+  console.log('Correct!');
+} else {
+  console.log('Incorrect!');
+}
+
+let favoritePhrase = 'Love That!';
+
+if (favoritePhrase === 'Love That!') {
+  console.log('I love that!');
+} else {
+  console.log("I don't love that!");
+}
