c51215906de148a7b90797946f395a2bbf07803d
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-05 18:44:02 +0530

    feat: Use Ternary Operator without using the if conditions

--- script/Script.js
+++ script/Script.js
@@ -9,12 +9,14 @@ if (isLocked) {
 isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
 
 let isCorrect = true;
-
+/*
 if (isCorrect) {
   console.log('Correct!');
 } else {
   console.log('Incorrect!');
 }
+*/
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
 
 let favoritePhrase = 'Love That!';
 
