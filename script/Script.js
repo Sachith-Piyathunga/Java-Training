55e241b42690cadc0c4b69ba22457f96843a6e44
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-06 12:50:36 +0530

    feat: Check scope pollution

--- script/Script.js
+++ script/Script.js
@@ -1,5 +1,11 @@
-const logVisibleLightWaves = () => {
-  const lightWaves = 'Moonlight';
-}
-console.log(lightWaves);
-logVisibleLightWaves();
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+const callMyNightSky = () => {
+  stars = 'Sirius';
+	return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+};
+
+console.log(callMyNightSky()); // the answer tis = Night Sky: The Moon, Sirius, The Milky Way
+console.log(stars); // Access the globale variable than the answer is = Sirius
