a9ed4460ccf6791b68d49de138bd65a2e26b9d96
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-06 20:34:57 +0530

    feat: Use loop througth array

--- script/Script.js
+++ script/Script.js
@@ -1,10 +1,6 @@
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
 // Write your code below
-for (let i = 5; i < 11; i++){
-  console.log(i);
-}
-
-
-// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
-for (let counter = 3; counter >= 0; counter--){
-  console.log(counter);
+for (let i = 0; i < vacationSpots.length; i++){
+  console.log('I would love to visit ' + vacationSpots[i]);
 }
