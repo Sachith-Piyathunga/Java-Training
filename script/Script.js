5bed541ef9cea79a04fa29b648b690d41846ea59
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-06 13:46:40 +0530

    feat: Try array with let anconst

--- script/Script.js
+++ script/Script.js
@@ -1,3 +1,12 @@
-let groceryList = ['bread', 'tomatoes', 'milk'];
-groceryList[1] = 'avocados';
-console.log(groceryList);
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
+
+condiments = ['Mayo'];
+console.log(condiments);
+
+utensils[3] = 'Spoon';
+console.log(utensils);
