ee0ae18ca7b97a3c44356de2846a4dc4d2755941
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-06 20:28:32 +0530

    feat: Check reverse loop

--- script/Script.js
+++ script/Script.js
@@ -2,3 +2,9 @@
 for (let i = 5; i < 11; i++){
   console.log(i);
 }
+
+
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+for (let counter = 3; counter >= 0; counter--){
+  console.log(counter);
+}
