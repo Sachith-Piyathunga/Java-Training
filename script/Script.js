a99a917df6d908af29729d6e4edb09ed825b836c
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-06 12:57:30 +0530

    feat: Practicing the good scope

--- script/Script.js
+++ script/Script.js
@@ -1,11 +1,11 @@
-const satellite = 'The Moon';
-const galaxy = 'The Milky Way';
-let stars = 'North Star';
-
-const callMyNightSky = () => {
-  stars = 'Sirius';
-	return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  // Add if statement here:
+  if (region === 'The Arctic') {
+    let lightWaves = 'Northern Lights';
+    console.log(lightWaves);
+  }
+  console.log(lightWaves);
 };
-
-console.log(callMyNightSky()); // the answer tis = Night Sky: The Moon, Sirius, The Milky Way
-console.log(stars); // Access the globale variable than the answer is = Sirius
+logVisibleLightWaves();
