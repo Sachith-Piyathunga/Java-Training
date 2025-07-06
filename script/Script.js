d6b817f4e9a273faac5b653e881a267a514d9cd5
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-06 21:05:33 +0530

    feat: Create a do...while loop

--- script/Script.js
+++ script/Script.js
@@ -1,9 +1,8 @@
-const cards = ['diamond', 'spade', 'heart', 'club'];
-
 // Write your code below
-let currentCard; // Declare the variable call currentCard
+const cupsOfSugarNeeded = 3;
+let cupsAdded = 0;
 
-while (currentCard !== 'spade') { // Create a while loop, that does not have value spade
-  currentCard = cards[Math.floor(Math.random() * 4)];
-	console.log(currentCard);
-}
+do {
+ cupsAdded++
+ console.log(cupsAdded + ' cup was added') 
+} while (cupsAdded < cupsOfSugarNeeded);
