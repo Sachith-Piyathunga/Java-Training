07d11d3492be46cd7517e16ca00fbc23cccf0bfe
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-06 13:47:46 +0530

    feat: Check the length of the property

--- script/Script.js
+++ script/Script.js
@@ -1,12 +1,2 @@
-let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
-
-const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
-
-condiments[0] = 'Mayo';
-console.log(condiments);
-
-condiments = ['Mayo'];
-console.log(condiments);
-
-utensils[3] = 'Spoon';
-console.log(utensils);
+const objectives = ['Learn a new language', 'Read 52 books', 'Run a marathon'];
+console.log(objectives.length);
