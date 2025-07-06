f3cfab26d89ef854a4b2a57aab67d6b0755511cb
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-06 13:27:31 +0530

    feat: Try basic js array

--- script/Script.js
+++ script/Script.js
@@ -1,3 +1,6 @@
 let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
 
 console.log(newYearsResolutions);
+const hobbies = ['Music', 'Cricket', 'Car'];
+
+console.log(hobbies);
