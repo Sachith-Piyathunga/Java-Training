95242752433bd28dc83f2aac35c4edf2899d8fc3
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-09 14:29:15 +0530

    feat: Try .reduce method

--- script/Script.js
+++ script/Script.js
@@ -1,2 +1,7 @@
-const newNumbers = [1, 3, 5, 7];
+const numbers = [1, 2, 4, 10];
 
+const summedNums = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue
+})
+
+console.log(summedNums) // Output: 17
