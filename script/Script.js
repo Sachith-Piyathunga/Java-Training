ed953ac412edee17a3e49bcf52dcb54800c71a7b
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-09 13:44:24 +0530

    feat: Add .findIndex method

--- script/Script.js
+++ script/Script.js
@@ -3,3 +3,7 @@ const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salaman
 const foundAnimal = animals.findIndex(animal => {
   return animal === 'elephant';
 });
+
+const startsWithS = animals.findIndex(animal => {
+  return animal[0] === 's' ? true : false;
+});
