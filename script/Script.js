1e5607c157327cc1b73a2e48fe22787d3111e6ae
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-09 14:35:05 +0530

    feat: Add new method

--- script/Script.js
+++ script/Script.js
@@ -1,8 +1,7 @@
 const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
 
 // Something is missing in the method call below
-
-console.log(words.some(() => {
+console.log(words.some(word => {
   return word.length < 6;
 }));
 
