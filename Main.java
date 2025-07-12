0ffaf00179c936854da02f5eeda35cb0ffe0aaea
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-12 11:20:00 +0530

    feat: Display the factores of 3

--- Main.java
+++ Main.java
@@ -1,24 +1,27 @@
 /**
- * Extract First Digit of Integer
- * Write a Java method for extracting the first digit from a positive or negative integer. 
-*/
+ * Display Factors of 3 in Integer
+ * Write a Java method to display the factors of 3 in a given integer.
+ */
 
-import java.util.Scanner;
+ import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+ public class Main {
+    public static void main (String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter an integer: ");
+        System.out.println("Enter the integer: ");
         int n = in.nextInt();
-        System.out.println("Extract the first digit from the said integer: ");
-        System.out.println(test(n));
+        System.out.println("Factors of 3 of the said integer: \n");
+        test(n);
     }
 
-    public static int test(int n) {
-        int fact_num = 10;
-        while(n / fact_num!= 0) {
-            fact_num *= 10;
+    public static void test(int n) {
+        System.out.println(n + " = ");
+        int result = n;
+        while (result % 3 == 0) {
+            System.out.println("3 * ");
+            result = result / 3;
         }
-        return Math.abs(n / (fact_num/10));
+        System.out.println(result);
     }
-}
+
+ }
