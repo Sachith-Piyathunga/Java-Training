13c58f4cf22b228fcc0c50ecb12f900eaf9b14f3
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-13 21:00:11 +0530

    feat: Complete the code

--- script/Script.js
+++ script/Script.js
@@ -11,3 +11,7 @@ if (today.getMonth() == 11 && today.getDate() > 25) {
   cmas.setFullYear(cmas.getFullYear() + 1);
 }
 
+var one_day = 1000 * 60 * 60 * 24;
+
+console.log(Math.ceil((cmas.getTime() - today.getTime()) / (one_day)) + " days left until Christmas!");
+
