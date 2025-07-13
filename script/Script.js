f778fbcca9397936ceafb279b577a5ce211775b0
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-13 17:24:58 +0530

    feat: Check for special cases when hour is 0

--- script/Script.js
+++ script/Script.js
@@ -17,4 +17,14 @@ var second = today.getSeconds();
 
 var prepand = (hour >= 12) ? " PM " : " AM ";
 
+hour = (hour >= 12) ? hour - 12 : hour;
 
+if (hour === 0 && prepand === ' PM ') {
+  if (minute === 0 && second === 0) {
+    hour = 12;
+    prepand = ' Noon';
+  } else {
+    hour = 12;
+    prepand = ' PM';
+  }
+}
