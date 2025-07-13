a9e2a42ccf6558ab6ea86eeaf709a36944a79b99
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-13 17:20:45 +0530

    feat: Diclate the variables

--- script/Script.js
+++ script/Script.js
@@ -9,4 +9,10 @@ var today = new Date();
 var day = today.getDay();
 var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+console.log("Today is: " + daylist[day] + ".");
+
+var hour = today.getHours();
+var minute = today.getMinutes();
+var second = today.getSeconds();
+
 
