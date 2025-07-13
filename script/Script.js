4c4c868a1a8c11645185a41558b5878776ba7e7c
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-13 20:55:56 +0530

    feat: Create a code calculate the days

--- script/Script.js
+++ script/Script.js
@@ -1,15 +1,6 @@
 /**
- * Check Leap Year (Gregorian Calendar)
- * Write a JavaScript program to determine whether a given year is a leap 
- * year in the Gregorian calendar.
+ * Days Left Before Christmas
+ * Write a JavaScript program to calculate the days left before Christmas.
  */
 
-function leapyear(year) {
-  return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
-}
-
-console.log(leapyear(2016));
-console.log(leapyear(2000));
-console.log(leapyear(1700));
-console.log(leapyear(1800));
-console.log(leapyear(100));
+today = new Date();
