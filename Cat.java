2a0d6b35ad7963b8d63c2c22236fd8ac12f346e3
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-14 13:15:38 +0530

    feat: Create a object and complete the code

--- Cat.java
+++ Cat.java
@@ -25,6 +25,9 @@
     }
 
     public static void main(String[] args) {
-        
+        Cat myCat = new Cat();
+
+        System.out.println("Cat's name: " + myCat.getName());
+        System.out.println("Cat's age: " + myCat.getAge());
     }
  }
