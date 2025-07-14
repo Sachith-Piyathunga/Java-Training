d289ba5b247d3e5ddf68ea1567d0ec1dfded4ba5
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-14 21:01:56 +0530

    feat: Create a new class

---
+++ Account.java
@@ -0,0 +1,17 @@
+/**
+ * Constructor with Validation
+ * Write a Java program to create a class called Account with instance variables 
+ * accountNumber and balance. Implement a parameterized constructor that initializes
+ *  these variables with validation:
+ * 
+ * accountNumber should be non-null and non-empty.
+ * balance should be non-negative.
+ * Print an error message if the validation fails.
+ */
+
+ public class Account {
+    
+    private String accountNumber;
+    private double balance;
+ }
+
