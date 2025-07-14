d8f9542ee7de590f0e277d1384d1ad73e61fd1ca
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-14 20:45:54 +0530

    feat: Create a parameterized constructor

--- Rectangle.java
+++ Rectangle.java
@@ -10,5 +10,9 @@ public class Rectangle {
    
     private double length;
     private double width;
-    
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
 }
