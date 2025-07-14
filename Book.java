66de29eef3409dc1e2080de8c9613b6b297e0913
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-14 13:28:05 +0530

    feat: create a class and add instance variables

---
+++ Book.java
@@ -0,0 +1,17 @@
+/**
+ * Constructor Overloading
+ * Write a Java program to create a class called "Book" with instance variables 
+ * title, author, and price. Implement a default constructor and two parameterized 
+ * constructors:
+ * 
+ * One constructor takes title and author as parameters.
+ * The other constructor takes title, author, and price as parameters.
+ * Print the values of the variables for each constructor.
+ */
+
+ public class Book {
+    
+    private String title;
+    private String author;
+    private double price;
+ }
