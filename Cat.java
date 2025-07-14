399c7d0764d0414e0ee86e26981cc297ad5538fa
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-14 13:12:29 +0530

    feat: Add getters and setters

--- Cat.java
+++ Cat.java
@@ -13,6 +13,14 @@
     public Cat() {
         this.name = "Unknown";
         this.age = 0;
-        
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
     }
  }
