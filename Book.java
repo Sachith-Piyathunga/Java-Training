bce130279aa5ea649c3a161bf9d2debbb02f9cb6
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-14 13:30:09 +0530

    feat: create defult constr4uctor

--- Book.java
+++ Book.java
@@ -14,4 +14,12 @@
     private String title;
     private String author;
     private double price;
+
+    public Book () {
+        this.title = "Unknown";
+        this.author = "Unknown";
+        this.price = 0.0;
+    }
+
+    
  }
