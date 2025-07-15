cdaf847ce9002ada77fabe70d0d0a749d3ba86b3
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-15 15:50:08 +0530

    feat: Complete the code

--- Singleton.java
+++ Singleton.java
@@ -27,6 +27,11 @@
 
         Singleton instance1 = Singleton.getInstance();
         Singleton instance2 = Singleton.getInstance();
-        
+
+        if (instance1 == instance2) {
+            System.out.println("Both instances are the same.");
+        } else {
+            System.out.println("Instances are different.");
+        }
     }
  }
