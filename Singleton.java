f6ed71c2cbea96283e68e384cc8c8777a3558c90
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-15 15:48:40 +0530

    feat: Get the single instance of Singleton

--- Singleton.java
+++ Singleton.java
@@ -25,5 +25,8 @@
 
     public static void main(String[] args) {
 
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+        
     }
  }
