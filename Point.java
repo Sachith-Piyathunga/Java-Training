d163fa50ef188bd3062bfc34f30ec014e72d6b6f
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-15 15:09:58 +0530

    feat: Create constructor that takes int parameters

--- Point.java
+++ Point.java
@@ -13,5 +13,10 @@ public class Point {
     
     private int x;
     private int y;
-    
+
+    public Point(int x, int y) {
+        
+        this.x = (int) x;
+        this.y = (int) y;
+    }
 }
