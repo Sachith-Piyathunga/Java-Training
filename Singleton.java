21335a5853b36939e0a1c575bc0dfbfc6a512f5b
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-15 15:40:29 +0530

    feat: Create a class

---
+++ Singleton.java
@@ -0,0 +1,13 @@
+/**
+ * Constructor for Singleton Pattern
+ *
+ *  Write a Java program to create a class called Singleton that ensures only 
+ * one instance of the class can be created. Implement a private constructor and 
+ * a public static method to get the single instance of the class. Print a message 
+ * indicating the creation of the instance.
+ * 
+ */
+
+ public class Singleton {
+    
+ }
