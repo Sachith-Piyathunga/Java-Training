2d71bfce18d44691d0904f61165934f9d1fffe16
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-15 15:07:36 +0530

    feat: Create a class called Point

---
+++ Point.java
@@ -0,0 +1,14 @@
+/**
+ * Overloading Constructors with Different Data Types
+ * 
+ * Write a Java program to create a class called Point with instance variables 
+ * x and y. Implement overloaded constructors:
+ * 
+ * One constructor takes int parameters.
+ * Another constructor takes double parameters.
+ * Print the values of the variables for each constructor.
+ */
+
+public class Point {
+    
+}
