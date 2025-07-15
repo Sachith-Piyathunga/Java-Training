d84a7d23c48cad17f4c07397d85f6b774684c31f
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-15 15:22:31 +0530

    feat: Create a class

--- Point.java
+++ Point.java
@@ -1,35 +1,12 @@
 /**
- * Overloading Constructors with Different Data Types
+ * Constructor with Array Initialization
  * 
- * Write a Java program to create a class called Point with instance variables 
- * x and y. Implement overloaded constructors:
- * 
- * One constructor takes int parameters.
- * Another constructor takes double parameters.
- * Print the values of the variables for each constructor.
+ * Write a Java program to create a class called Classroom with instance 
+ * variables className and students (an array of strings). Implement a 
+ * parameterized constructor that initializes these variables. Print the 
+ * values of the variables.
  */
 
-public class Point {
+ public class Classroom {
     
-    private int x;
-    private int y;
-
-    public Point(int x, int y) {
-        
-        this.x = (int) x;
-        this.y = (int) y;
-    }
-
-    public void printPoint() {
-        System.out.println("Point (x, y): (" + x + "," + y + ")");
-    }
-
-    public static void main(String[] args) {
-
-        Point point1 = new Point(4, 6);
-        point1.printPoint();
-
-        Point point2 = new Point(4.5, 5.5);
-        point2.printPoint();
-    }
-}
+ }
