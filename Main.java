c710ae447729198702b1cadd60a19c28d4dad9da
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-16 21:33:13 +0530

    feat: Overloaded method (same name, different parameter)

--- Main.java
+++ Main.java
@@ -6,4 +6,8 @@ class Parent {
     public void func() {
         System.out.println("Parent.func()");
     }
+
+    public void func(int a) {
+        System.out.println("Parent func(int): " + a);
+    }
 }
