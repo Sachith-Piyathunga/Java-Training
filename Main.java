84af31dee688c72acea5320196edd4c06d473585
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-16 21:31:28 +0530

    feat: Overloaded method (compile-time polymorphism)

--- Main.java
+++ Main.java
@@ -1,28 +1,9 @@
-// Inheritance 
+// Polymorphism
+// Method Overloading and Overriding
 
-class Animal {
-    
-    void eat() {
-        System.out.println("Animal is eating.....");
-    }
-    void sleep() {
-        System.out.println("Animal is sleepnig.....");
-    }
-}
+class Parent {
 
-class Dog extends Animal {
-        void bark() {
-            System.out.println("Dog is barking.....");
-        }   
-    }
-
-public class Main {
-    public static void main(String[] rgs) {
-        Dog myDog = new Dog();
-
-        myDog.eat();
-        myDog.sleep();
-
-        myDog.bark();
+    public void func() {
+        System.out.println("Parent.func()");
     }
 }
