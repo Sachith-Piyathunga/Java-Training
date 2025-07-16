64de63d5be79a0523a1247b0e8c4bff3339a39e4
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-16 18:29:37 +0530

    style: Constructor for initialization

--- Student.java
+++ Student.java
@@ -2,5 +2,9 @@ public class Student {
     
     private int ID;
     private String name;
-    
+
+    public Student(int ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
 }
