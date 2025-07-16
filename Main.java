6468be834ea327de79079d88e566fee00a78b280
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-16 21:35:26 +0530

    feat: Overrides Parent.func(int) (runtime polymorphism)

--- Main.java
+++ Main.java
@@ -1,13 +1,23 @@
 // Polymorphism
 // Method Overloading and Overriding
 
+// Parent Class
 class Parent {
-
+    // Overloaded method (compile-time polymorphism)
     public void func() {
         System.out.println("Parent.func()");
     }
 
+    // Overloaded method (same name, different parameter)
     public void func(int a) {
-        System.out.println("Parent func(int): " + a);
+        System.out.println("Parent.func(int): " + a);
+    }
+}
+
+// child class
+class child extends Parent {
+    @Override
+    public void func(int a) {
+        System.out.println("Child.func(int): " + a);
     }
 }
