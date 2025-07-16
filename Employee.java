9d8992cd917308a021abbf6ca32fa84bd120f089
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-16 18:15:00 +0530

    feat: Create parameterized constructor

--- Employee.java
+++ Employee.java
@@ -3,5 +3,9 @@ public class Employee {
     private String name;
     private double salary;
 
-    
+    public Employee(String name, double salary) {
+
+        this.name =name;
+        this.salary = salary;
+    }
 }
