da7d60e1e57b53a230943263bbb482e7c4e01507
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-16 21:23:13 +0530

    feat: Complete the code

--- Main.java
+++ Main.java
@@ -8,18 +8,21 @@ class Animal {
     void sleep() {
         System.out.println("Animal is sleepnig.....");
     }
+}
 
-    class Dog extends Animal {
+class Dog extends Animal {
         void bark() {
             System.out.println("Dog is barking.....");
         }   
     }
-}
 
 public class Main {
     public static void main(String[] rgs) {
         Dog myDog = new Dog();
 
         myDog.eat();
+        myDog.sleep();
+
+        myDog.bark();
     }
 }
