549da5bdbad527c198a27c5072e7e20531ab9bc5
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-18 20:33:19 +0530

    feat: Create a class called Counter

---
+++ Counter.java
@@ -0,0 +1,12 @@
+/**
+ * Static Variables
+ * 
+ * Write a Java program to create a class called "Counter" with a static variable 
+ * count. Implement a constructor that increments count every time an object is 
+ * created. Print the value of count after creating several objects.
+ */
+
+ public class Counter {
+
+    
+ }
