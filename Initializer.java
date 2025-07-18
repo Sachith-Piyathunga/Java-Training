143fbd3daa7e9802fc4648dd9720587b65ebbaea
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-18 20:53:04 +0530

    feat: Define the Initializer class

---
+++ Initializer.java
@@ -0,0 +1,12 @@
+/**
+ * Static Block
+ * 
+ * Write a Java program to create a class called "Initializer" with a static 
+ * block that initializes a static variable 'initialValue' to 1000. Print the 
+ * value of 'initialValue' before and after creating an instance of "Initializer".
+ * 
+ */
+
+ public class Initializer {
+    
+ }
