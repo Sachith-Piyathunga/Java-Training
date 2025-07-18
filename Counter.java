05a940ce3946080d288ae9dd0884f5cf796b6807
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-18 20:38:49 +0530

    feat: Create several Counter objects

--- Counter.java
+++ Counter.java
@@ -19,6 +19,9 @@
     }
 
     public static void main(String[] args) {
-        
+
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
     }
  }
