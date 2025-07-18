744c1d9be8be1e5c982dd5df619b6ecd7b614b32
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-18 20:54:35 +0530

    feat: Static block to initialize initialValue

--- Initializer.java
+++ Initializer.java
@@ -10,4 +10,9 @@
  public class Initializer {
     
     static int initialValue;
+
+    static {
+        initialValue = 1000;
+        System.out.println("Static block: initialValue initialized to " + initialValue);
+    }
  }
