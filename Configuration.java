6fac17e49ee37b6e73eed7f533b81acb4fe2ff30
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-19 14:08:02 +0530

    feat: Define the Configuration class

---
+++ Configuration.java
@@ -0,0 +1,13 @@
+/**
+ * Static Block for Initialization
+ * 
+ * Write a Java program to create a class called Configuration with a static 
+ * block that initializes a static variable 'configValue' from a configuration 
+ * file (simulated with a default value). Print the value of 'configValue' in 
+ * the main method.
+ * 
+ */
+
+public class Configuration  {
+    
+}
