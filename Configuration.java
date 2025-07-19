c9a739a2f853646f497ea001d2783a81caff8fac
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-19 14:11:35 +0530

    feat: Print the value of configValue

--- Configuration.java
+++ Configuration.java
@@ -8,16 +8,21 @@
  * 
  */
 
-public class Configuration  {
-    
-    public static String Configuration;
+// Define the Configuration class
+public class Configuration {
+    // Declare a static variable configValue
+    public static String configValue;
 
+    // Static block to initialize configValue
     static {
-        configValue = "Default config value";
+        // Simulating reading from a configuration file by assigning a default value
+        configValue = "Default Config Value";
         System.out.println("Static block executed: configValue initialized");
     }
 
+    // Main method to print the value of configValue
     public static void main(String[] args) {
-        
+        // Print the value of configValue
+        System.out.println("Config Value: " + configValue);
     }
 }
