defbecabd647a0b0d7d2cfd71a5f34c03e142f2a
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-19 14:25:00 +0530

    feat: Define the IDGenerator class

---
+++ IDGenerator.java
@@ -0,0 +1,12 @@
+/**
+ * Static Method with Static Variable
+ * 
+ * Write a Java program to create a class called "IDGenerator" with a static 
+ * variable 'nextID' and a static method "generateID()" that returns the next ID 
+ * and increments 'nextID'. Demonstrate the usage of generateID in the main method.
+ * 
+ */
+
+public class IDGenerator {
+    
+}
