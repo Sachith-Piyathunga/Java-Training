2dcce525d256efcc7d55d135bcbfd15b0a357e36
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-20 17:01:16 +0530

    feat: Create a main2 class

--- Main2.java
+++ Main2.java
@@ -13,4 +13,12 @@ public class Main2 {
     HEXAGON
   }
 
+  public static void main(String[] args) {
+    Shape shape1 = Shape.CIRCLE;
+    Shape shape2 = Shape.SQUARE;
+
+    System.out.println("First shape: " + shape1);
+    System.out.println("Second shape: " + shape2);
+  }
+
 }
