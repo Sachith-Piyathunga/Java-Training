4b1e26d4376172361aaa99942098c27c8825d2ca
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-20 17:03:59 +0530

    feat: Complete the code

--- Main3.java
+++ Main3.java
@@ -8,7 +8,11 @@ public class Main3 {
   }
 
   public static void main(String[] args) {
-    
+    Direction direction1 = Direction.NORTH;
+    Direction direction2 = Direction.WEST;
+
+    System.out.println("First direction: " + direction1);
+    System.out.println("Second direction: " + direction2);
   }
 
 }
