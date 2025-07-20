61b0c8cbdd2f1e12edf59d68a0414065901add4e
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-20 17:05:08 +0530

    feat: Set variables

--- Main4.java
+++ Main4.java
@@ -1,3 +1,16 @@
 public class Main4 {
-    
+    public enum Months {
+     JANUARY,
+     FEBRUARY,
+     MARCH,
+     APRIL,
+     MAY,
+     JUNE,
+     JULY,
+     AUGUST,
+     SEPTEMBER,
+     OCTOBER,
+     NOVEMBER,
+     DECEMBER
+   }
 }
