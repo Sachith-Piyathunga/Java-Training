20915b7ea1fd03ea0c056a0e71214d5db986514a
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-20 16:58:44 +0530

    feat: Create a main1 class

---
+++ Main1.java
@@ -0,0 +1,16 @@
+/**
+ * Write a Java program to create an enum called "DaysOfWeek" 
+ * representing the days of the week.
+ * 
+ */
+
+ public class Main1 {
+  public enum Days_Of_Week {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+  }
