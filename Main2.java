0b0d4198c6cd9f3abcd67adae00b829efa1a4a3f
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-20 17:00:58 +0530

    feat: Create a main2 class

---
+++ Main2.java
@@ -0,0 +1,16 @@
+/**
+ * Write a Java program to implement an enum called 
+ * "Shape" with constants representing different geometric 
+ * shapes.
+ */
+
+public class Main2 {
+  public enum Shape {
+    SQUARE,
+    CIRCLE,
+    RECTANGLE,
+    TRIANGLE,
+    HEXAGON
+  }
+
+}
