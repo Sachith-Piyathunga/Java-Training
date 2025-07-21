1cd8ff9449573f5c89805731c9d96f9afcde270e
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-21 23:03:25 +0530

    feat: Add conditional stetment

--- Example3.java
+++ Example3.java
@@ -8,6 +8,9 @@ import java.util.*;
 
 public class Example3 {
     public static void main(String[] args) {
-        
+        double num = 5.4444;
+        if ((num % 1) == 0) {
+            System.out.println("It is not a double number");
+        }
     }
 }
