3eb5f9b79c4b2d2e2cc5dea90bcb8066d0bfe72b
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-21 22:55:09 +0530

    feat: Add all variables

--- Example2.java
+++ Example2.java
@@ -10,6 +10,9 @@
  public class Example2 {
     
     public static void main(String[] args) {
-
+        double value = 12.56;
+        double fractional_part = value % 1;
+        double integral_part = value - fractional_part;
+        
     }
  }
