863011afea88803098daa7b3f2b564e63236e03c
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-21 22:46:56 +0530

    feat: Add he problem stetment

--- Example1.java
+++ Example1.java
@@ -1,18 +1,5 @@
 /**
- * Round Up Integer Division
+ * Split Whole and Fractional Parts
  * 
- * Write a Java program to round up integer division results.
+ * Write a Java program to get whole and fractional parts from a double value.
  */
-
-import java.util.*;
-
-
-public class Example1 {
-    
-    public static void main(String[] args) {
-        int tot_theory_marks = 350, tot_practical_marks = 90, tot_marks = 500;
-        int percentage_of_marks = ((tot_theory_marks + tot_practical_marks)*100)/tot_marks;
-
-        System.out.println("\nPercentage of Marks: " + percentage_of_marks + "%\n");
-    }
-}
