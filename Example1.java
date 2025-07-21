7d5c8a65cf5a5a34e721dafe025efc0d9ea1dfce
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-21 22:47:53 +0530

    feat: Complete the code

--- Example1.java
+++ Example1.java
@@ -1,5 +1,19 @@
 /**
- * Split Whole and Fractional Parts
+ * Round Up Integer Division
  * 
- * Write a Java program to get whole and fractional parts from a double value.
+ * Write a Java program to round up integer division results.
  */
+
+import java.util.*;
+
+
+public class Example1 {
+    
+    public static void main(String[] args) {
+        
+        int tot_theory_marks = 350, tot_practical_marks = 90, tot_marks = 500;
+        int percentage_of_marks = ((tot_theory_marks + tot_practical_marks)*100)/tot_marks;
+
+        System.out.println("\nPercentage of Marks: " + percentage_of_marks + "%\n");
+    }
+}
