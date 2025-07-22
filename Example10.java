306df15d552761045ef7e9f1daacf6b18d4312ef
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-22 19:40:23 +0530

    feat: Create a class

---
+++ Example10.java
@@ -0,0 +1,14 @@
+/**
+ * Round Float Value
+ * 
+ * Write a Java program to accept a float value of a number and return a rounded float value.
+ * Sample data:
+ * Input a float number: 12.51
+ * The rounded value of 12.510000 is: 13.00
+ * Input a float number: 12.49999
+ * The rounded value of 12.499990 is: 12.00
+ */
+
+public class Example10 {
+    
+}
