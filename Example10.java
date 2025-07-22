4e334cd3069686d8b92af983177260eed95c7bb1
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-22 19:42:22 +0530

    feat: Create the main method

--- Example10.java
+++ Example10.java
@@ -8,7 +8,12 @@
  * Input a float number: 12.49999
  * The rounded value of 12.499990 is: 12.00
  */
+import java.util.Scanner;
 
 public class Example10 {
-    
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input a float number: ");
+    }
 }
