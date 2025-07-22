f1945230605e72a52dc9f321945739a0e1c5cb8b
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-22 19:35:33 +0530

    feat: Add print statements

--- Example9.java
+++ Example9.java
@@ -10,6 +10,9 @@ public class Example9 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        
+        System.out.println("Input a float number: ");
+        float x = in.nextFloat();
+        System.out.printf("The absol;ute value of %.2f is: %.2f", x ,convert(x));
+        System.out.printf("\n");
     }
 }
