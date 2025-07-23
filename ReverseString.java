c0103abaeead1b5221a9cb9181aa01442444d887
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-23 18:47:02 +0530

    feat: Complete the code

--- ReverseString.java
+++ ReverseString.java
@@ -1,5 +1,8 @@
 public class ReverseString {
     public static void main(String[] args) {
-        
+
+        String input = "Hello";
+        StringBuilder reversed = new StringBuilder(input).reverse();
+        System.out.println("Reversed: " + reversed);
     }
 }
