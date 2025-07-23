daf50af672f0b35849da223449c79a58a7aad489
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-23 20:24:02 +0530

    feat: Add the condition

--- Palindrome.java
+++ Palindrome.java
@@ -1,5 +1,11 @@
 public class Palindrome {
     public static boolean isPalindrome(String str) {
-        
+        int i = 0, j  = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i++) != str.charAt(j--)) return false;
+                 
+        }
+        return true;
     }
 }
+
