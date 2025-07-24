24f060ed401c438c0ebc6d0429ecc44b1c3a876c
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-24 10:31:53 +0530

    feat: Create the nested loop

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,6 +6,11 @@ public class BubbleSort {
 
         int[] arr = {4, 5, 6, 7,8};
 
+        for(int i = 0; i < arr.length - 1; i++) {
+            for(int j = 0; j <arr.length -i -1; j++) {
+                
+            }
+        }
     }
     
 }
