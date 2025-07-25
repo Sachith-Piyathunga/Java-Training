8d05a660cec8cf27d98c8a094717858fd1120ae4
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-25 17:05:03 +0530

    feat: Add array

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,5 +1,7 @@
 public class SecondLargest {
     public static void main(String[] args) {
-        
+        int[] arr = {10, 5, 20, 8}; 
+        int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+
     }
 }
