b7c6efa3af7c03832b82cec96989b77e328a116f
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-26 07:24:35 +0530

    feat: Add Default constructor

--- Book.java
+++ Book.java
@@ -2,5 +2,11 @@ public class Book {
     private String title;
     private String author;
     private double price;
+
+    public Book () {
+        this.title = "Unknown";
+        this.author = "Unknown";
+        this.price = 0.0;
+    }
     
 }
