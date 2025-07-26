b76e69a206c11b9a0810b54335358155aaf29898
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-26 07:08:09 +0530

    feat: Complete the code

--- RemoveVowels.java
+++ RemoveVowels.java
@@ -2,6 +2,7 @@ public class RemoveVowels {
     public static void main(String[] args) {
         String input = "beautiful";
         String result = input.replaceAll("{aeiouAEIOU}", "");
-        
+
+        System.out.println("Without vowels: " + result);
     }
 }
