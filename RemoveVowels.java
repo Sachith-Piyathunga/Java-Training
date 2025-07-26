8647a130994aa134c299df3630874bd1e37dad45
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-26 07:07:18 +0530

    feat: Create the main method

--- RemoveVowels.java
+++ RemoveVowels.java
@@ -1,5 +1,7 @@
 public class RemoveVowels {
     public static void main(String[] args) {
+        String input = "beautiful";
+        String result = input.replaceAll("{aeiouAEIOU}", "");
         
     }
 }
