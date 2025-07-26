9fc329dab06d6325dd5496ad419a426371548b7d
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-26 07:26:23 +0530

    feat: Add Parameterized constructor (title, author)

--- Book.java
+++ Book.java
@@ -3,10 +3,15 @@ public class Book {
     private String author;
     private double price;
 
-    public Book () {
+    public Book() {
         this.title = "Unknown";
         this.author = "Unknown";
         this.price = 0.0;
     }
-    
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.price = 0.0;
+    }
 }
