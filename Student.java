7f58c516ef9916d7fe83f38376dc19e67c812e13
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-26 07:14:32 +0530

    feat: Complete the code

--- Student.java
+++ Student.java
@@ -13,6 +13,8 @@ public class Student {
     }
 
     public static void main(String[] args) {
-        
+
+        Student student1 = new Student("Sachintha", 25);
+        student1.printDetails();
     }
 }
