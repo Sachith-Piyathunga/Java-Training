a3acc126067f70af4e59a37d2c16d25103a251c0
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-27 20:59:09 +0530

    feat: Create a class called Library

---
+++ Library.java
@@ -0,0 +1,12 @@
+/**
+ * Inner Class Accessing Outer Class Members
+ * 
+ * Write a Java program to create an outer class called Library with an instance 
+ * variable libraryName. Create an inner class Book with a method getLibraryName() 
+ * that returns the name of the library. Instantiate the Book class and call the 
+ * getLibraryName() method.
+ */
+
+public class Library {
+    
+}
