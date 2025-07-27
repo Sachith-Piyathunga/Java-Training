f9aa1e7e510f4506ad77bd6137a2dc806c210f75
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-27 20:36:33 +0530

    feat: Create outter class

---
+++ Car.java
@@ -0,0 +1,12 @@
+/**
+ * Local Class
+ * 
+ * Write a Java program to create a class called Car with a method startEngine(). 
+ * Inside this method, define a local class Engine that has a method run(). The run() 
+ * method should print "Engine is running". Instantiate and call the run() method from 
+ * within the startEngine() method.
+ */
+
+public class Car {
+    
+}
