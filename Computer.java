4024a4efeb2cac33a15172fdda5fac76f65d8cb0
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-27 20:13:29 +0530

    feat: Method to display processor details

--- Computer.java
+++ Computer.java
@@ -1,6 +1,9 @@
 class Computer {
     
     class Processor {
-        
+        void displayDetails() {
+            System.out.println("Processor Brand: Intel");
+            System.out.println("Processor Speed: 3.5 GHz");
+        }
     }
 }
