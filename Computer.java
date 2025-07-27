79cfc91080618f89fecdcb9bd5983f73ce7f22d7
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-27 20:14:55 +0530

    feat: Method to create and display Processor details

--- Computer.java
+++ Computer.java
@@ -1,3 +1,12 @@
+/**
+ * Inner Class
+ * 
+ * Write a Java program to create an outer class called Computer with an inner 
+ * class Processor. The Processor class should have a method "displayDetails()" 
+ * that prints the details of the processor (e.g., brand and speed). Create an 
+ * instance of Processor from the Computer class and call the "displayDetails()" method
+ */
+
 class Computer {
     
     class Processor {
@@ -5,5 +14,9 @@ class Computer {
             System.out.println("Processor Brand: Intel");
             System.out.println("Processor Speed: 3.5 GHz");
         }
+    }
+
+    void showProcessorDetails() {
+        
     }
 }
