46f1784ed434d18dc61c12f347a35a4c4845e00c
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-27 20:49:06 +0530

    feat: Create a class

---
+++ Anonymous.java
@@ -0,0 +1,11 @@
+/**
+ * Anonymous Class Exercise
+ * 
+ * Write a Java program to create an interface called Greeting with a method sayHello(). 
+ * In the main method, create an anonymous class that implements the Greeting interface 
+ * and override the sayHello() method to print "Hello, World!". Call the sayHello() method.
+ */
+
+public class Anonymous {
+    
+}
