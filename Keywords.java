f8c1957d47b04a5719a5311a5b4d8fcc413bdd22
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-30 20:44:18 +0530

    feat: Add comment

---
+++ Keywords.java
@@ -0,0 +1,9 @@
+public class Keywords {
+    
+}
+
+
+/**
+ * abstract - Specifics that a class or a method will be implimented later.
+ * boolean - a data type that can hold True and False values only.
+ */
