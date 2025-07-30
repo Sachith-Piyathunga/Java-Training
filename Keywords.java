f241ed00c8dd3d8261594b18545de0bef5611e8b
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-30 20:51:45 +0530

    feat: Add comment

--- Keywords.java
+++ Keywords.java
@@ -12,5 +12,7 @@ public class Keywords {
  * default - Specifies the default block of code in a switch statement.
  * else - Indicates alternative branches in an if statement.
  * enum	- A Java keyword is used to declare an enumerated type. Enumerations extend the base class.
- * extends - Indicates that a class is derived from another class or interface 
+ * extends - Indicates that a class is derived from another class or interface.
+ * final - Indicates that a variable holds a constant value or that a method will not be overridden.
+ * finally - Indicates a block of code in a try-catch structure that will always be executed
  */
