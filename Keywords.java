abc91d81c2875da924dd29f5b51c79f0d058025b
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-07-30 20:45:19 +0530

    feat: Add comment

--- Keywords.java
+++ Keywords.java
@@ -6,4 +6,5 @@ public class Keywords {
 /**
  * abstract - Specifics that a class or a method will be implimented later.
  * boolean - a data type that can hold True and False values only.
+ * breake - A contral statement for braking out the loops.
  */
