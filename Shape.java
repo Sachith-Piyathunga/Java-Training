c764c5eacb84775bf6cd448590a4a55280aa5fe1
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-01 22:03:07 +0530

    feat: Create an object

--- Shape.java
+++ Shape.java
@@ -77,11 +77,11 @@ public class Triangle implements Shape {
 
 public class Shape {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(10,12):
+        Rectangle rectangle = new Rectangle(10,12);
 
         Circle circle = new Circle(3);
 
-        
+        Triangle triangle = new Triangle(4, 6);
     }
 
 }
