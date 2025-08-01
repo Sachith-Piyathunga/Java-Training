9fabb7f20c444264ad428b26ca104a1c7c0b6e02
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-01 22:00:21 +0530

    feat: Complete the Triangle class

--- Shape.java
+++ Shape.java
@@ -53,18 +53,25 @@ public class Circle implements Shape {
     }
 }
 
+// Triangle.java
+// This is the 'Triangle' class that implements the 'Shape' interface.
+
 public class Triangle implements Shape {
+    // Private instance variables to store the base and height of the triangle.
     private double base;
     private double height;
 
+    // Constructor for creating a Triangle object with a given base and height.
     public Triangle(double base, double height) {
         this.base = base;
         this.height = height;
     }
 
+    // Implementation of the 'getArea' method as required by the 'Shape' interface.
     @Override
     public double getArea() {
-        
+        // Calculate and return the area of the triangle using the formula: 0.5 * base * height.
+        return 0.5 * base * height;
     }
 }
 
