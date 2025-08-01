835dd9f8ec3dd2fce198977b154ef338239bbbf9
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-01 22:04:50 +0530

    feat: Complete the full code

--- Shape.java
+++ Shape.java
@@ -77,15 +77,23 @@ public class Triangle implements Shape {
 
 public class Shape {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(10,12);
-
+        // Create a Rectangle object with length=10 and width=12
+        Rectangle rectangle = new Rectangle(10, 12);
+        
+        // Create a Circle object with radius=3
         Circle circle = new Circle(3);
-
+        
+        // Create a Triangle object with base=4 and height=6
         Triangle triangle = new Triangle(4, 6);
 
+        // Print the area of the Rectangle
         System.out.println("Area of the Rectangle: " + rectangle.getArea());
+        
+        // Print the area of the Circle
         System.out.println("Area of the Circle: " + circle.getArea());
         
+        // Print the area of the Triangle
+        System.out.println("Area of the Triangle: " + triangle.getArea());
     }
 
 }
