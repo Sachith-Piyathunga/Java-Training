cd0fa23e4bb8e172bf4e132583351896f676a4e9
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-01 21:46:39 +0530

    feat: Add parameterized constructor

--- Shape.java
+++ Shape.java
@@ -15,6 +15,10 @@ public class Rectangle implements Shape {
 
     private double length;
     private double width;
+
+    public Rectangle(double length, double width) {
+        
+    }
 }
 
 public class Shape {
