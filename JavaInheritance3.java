aa10e8d5bd5a0b33f76548de44268990871b8578
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-06 18:49:34 +0530

    feat: Add the problem stetment

---
+++ JavaInheritance3.java
@@ -0,0 +1,10 @@
+/**
+ * Write a Java program to create a class called Employee with methods called work() 
+ * and getSalary(). Create a subclass called HRManager that overrides the work() 
+ * method and adds a new method called addEmployee().
+ * 
+ */
+
+public class JavaInheritance3 {
+    
+}
