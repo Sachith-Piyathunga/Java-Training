40b95a8b40bb1403f9285ed1bb6128875e3898c8
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-06 20:32:47 +0530

    feat: Add the condition

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -53,6 +53,8 @@ public class SavingsAccount extends BankAccount {
     public void withdraw(double amount) {
         if(getBalance() - amount < 100) {
             System.out.println("Minimum balance of $100 required!");
+        } else {
+            super.withdraw(amount);
         }
     }
 }
