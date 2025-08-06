12bd2d41c11dca6ea38555c78e66ccea4e0d4c48
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-06 20:01:44 +0530

    feat: Create the constructor

--- JavaInheritance4.java
+++ JavaInheritance4.java
@@ -4,9 +4,15 @@
  * withdraw() method to prevent withdrawals if the account balance falls below one hundred.
  */
 
+ // Declare the BankAccount class
 class BankAccount {
     private String accountNumber;
     private double blance;
+
+    public BankAccount(String accountNumber, double blance) {
+        this.accountNumber = accountNumber;
+        this.blance = blance;
+    }
 }
 
 public class JavaInheritance4 {
