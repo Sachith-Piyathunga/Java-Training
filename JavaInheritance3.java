bb82a60a859e90ccaa27e41a578176abd22fe308
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-06 18:51:30 +0530

    feat: Create ther class

--- JavaInheritance3.java
+++ JavaInheritance3.java
@@ -1,9 +1,14 @@
 /**
- * Write a Java program to create a class called Employee with methods called work() 
+ * Write a Java program to create a class called Employee(JavaInheritance3) with methods called work() 
  * and getSalary(). Create a subclass called HRManager that overrides the work() 
  * method and adds a new method called addEmployee().
  * 
  */
+
+class Employee {
+    private double salary;
+    
+}
 
 public class JavaInheritance3 {
     
