2c277ef57d88a40935a03cdf3e4479f959fcad8e
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-06 19:26:30 +0530

    feat: Create the main method

--- HRManager.java
+++ HRManager.java
@@ -30,20 +30,34 @@ class Employee {
 } 
 
 // HRManager.java class
-
+// HRManager.java
+// Child class HRManager
 public class HRManager extends Employee {
-    public HRManager(double salary) {
+
+    // Constructor to initialize the salary of the HRManager
+    public HRManager(int salary) {
+        // Call the parent class constructor with the salary
         super(salary);
     }
+
+    // Overridden method to simulate the HRManager working
     public void work() {
-        System.out.println("\nManaging Employee");
+        // Print a message indicating the HRManager is managing employees
+        System.out.println("\nManaging employees");
     }
 
+    // Method to simulate adding a new employee
     public void addEmployee() {
-        System.out.println("\nAdding new Employee");
+        // Print a message indicating a new employee is being added
+        System.out.println("\nAdding new employee!");
     }
 }
 
+
 public class JavaInheritance3 {
+
+    public static void main(String[] args) {
+        
+    }
     
 }
