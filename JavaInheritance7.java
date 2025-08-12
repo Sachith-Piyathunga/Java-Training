5801af933c280915f4dd6f6a9f3354244327b7f7
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-12 21:55:31 +0530

    feat: Add the problem stetment

---
+++ JavaInheritance7.java
@@ -0,0 +1,10 @@
+/**
+ * Write a Java program to create a class known as "BankAccount" with methods called 
+ * deposit() and withdraw(). Create a subclass called SavingsAccount that overrides the 
+ * withdraw() method to prevent withdrawals if the account balance falls below one hundred.
+ */
+
+
+public class JavaInheritance7 {
+    
+}
