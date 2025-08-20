8e1b62b6900eb876d3ac16a9f186b54b99ec1cbe
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-20 20:25:52 +0530

    feat: Get values using getter methods

--- JavaEncapsulation.java
+++ JavaEncapsulation.java
@@ -57,5 +57,8 @@ public class JavaEncapsulation {
         person.setAge(25);
         person.setCountry("Sri Lanaka");
 
+        // Get values using getter methods
+        String name = person.getName();
+
     }
 }
