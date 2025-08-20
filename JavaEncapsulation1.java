e9e15f7fc6baf425b45e637fa977d4bd550393ae
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-20 20:59:20 +0530

    feat: Add probalem statement

---
+++ JavaEncapsulation1.java
@@ -0,0 +1,11 @@
+/**
+ * Write a Java program to create a class called BankAccount with private instance 
+ * variables accountNumber and balance. Provide public getter and setter methods to 
+ * access and modify these variables.
+ */
+
+
+
+public class JavaEncapsulation1 {
+    
+}
