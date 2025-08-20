fd079d4653278549d4468e6fec5999c9d1276e15
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-20 20:04:30 +0530

    feat: Create the person class

---
+++ JavaEncapsulation.java
@@ -0,0 +1,15 @@
+/**
+ * Write a Java program to create a class called Person with private instance variables 
+ * name, age. and country. Provide public getter and setter methods to access and modify these variables.
+ */
+ 
+// Person.java
+// Person class
+class Person {
+
+}
+
+
+public class JavaEncapsulation {
+    
+}
