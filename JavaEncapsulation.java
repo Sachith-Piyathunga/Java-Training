2b81d1fb606a5fd94d79f1692e22263edae55a6a
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-20 20:28:11 +0530

    feat: Complete the code

--- JavaEncapsulation.java
+++ JavaEncapsulation.java
@@ -62,5 +62,10 @@ public class JavaEncapsulation {
         int age = person.getAge();
         String country = person.getCountry();
 
+        // Print the values
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Country: " + country);
+
     }
 }
