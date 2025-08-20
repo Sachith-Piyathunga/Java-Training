86298a3d413979bacb0b49ce4fd18856bac5c8b6
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-20 20:08:16 +0530

    feat: Create a private field

--- JavaEncapsulation.java
+++ JavaEncapsulation.java
@@ -6,7 +6,16 @@
 // Person.java
 // Person class
 class Person {
+    // Private field to store the name of the person
+    private String name;
+    // Private field to store the age of the person
+    private int age;
+    // Private field to store country of the person
+    private String country;
 
+
+
+    
 }
 
 
