1badaca289ba6f531fc343307de73a9e5885548f
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-21 20:44:44 +0530

    feat: Add the grade to the graeds list

--- JavaEncapsulation3.java
+++ JavaEncapsulation3.java
@@ -52,6 +52,8 @@ class Student {
         if (grades == null) {
             grades = new ArrayList<>();
         }
+        // Add the grade to the graeds list
+        grades.add(grade);
     }
 }
 
