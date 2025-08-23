e5b6789494d1ae32bffb99c7219b2c0e2f3645cd
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-23 08:45:30 +0530

    feat: Create the class called movie

---
+++ JavaEncapsulation7.java
@@ -0,0 +1,16 @@
+/**
+ * Write a Java program to create a class called Movie with private instance variables 
+ * title, director, and duration. Provide public getter and setter methods to access 
+ * and modify these variables. Add a method called getMovieDetails() that returns a 
+ * formatted string containing the movie details.
+ */
+
+// Define the Movive class
+/* public */ class Movie {
+    
+}
+
+
+public class JavaEncapsulation7 {
+    
+}
