59b4d44995032650df0da9d058c4951a14c5b76b
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-27 19:51:39 +0530

    feat: Add the problem statement

---
+++ JavaPolymorphism4.java
@@ -0,0 +1,11 @@
+/**
+ * Write a Java program to create a class Employee with a method called calculateSalary(). 
+ * Create two subclasses Manager and Programmer. In each subclass, override the 
+ * calculateSalary() method to calculate and return the salary based on their 
+ * specific roles.
+ */
+
+
+public class JavaPolymorphism4 {
+    
+}
