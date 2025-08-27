951fa3258c0a7c03179715fd7172482f22b59259
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-27 20:34:07 +0530

    feat: Add the problem statement

---
+++ JavaPolymorphism5.java
@@ -0,0 +1,16 @@
+/**
+ * Write a Java program to create a base class Sports with a method called play(). 
+ * Create three subclasses: Football, Basketball, and Rugby. Override the play() 
+ * method in each subclass to play a specific statement for each sport.
+ */
+
+
+
+
+
+
+
+
+public class JavaPolymorphism5 {
+    
+}
