cb88ef69e8fd6cc953fd0a9adfe9736fa36fac52
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-30 17:12:42 +0530

    feat: Add the problem statement

---
+++ JavaPolymorphism8.java
@@ -0,0 +1,17 @@
+/**
+ * Write a Java program to create a base class Vehicle with methods startEngine() and 
+ * stopEngine(). Create two subclasses Car and Motorcycle. Override the startEngine() 
+ * and stopEngine() methods in each subclass to start and stop the engines differently.
+ */
+
+
+
+
+
+
+
+
+
+public class JavaPolymorphism8 {
+    
+}
