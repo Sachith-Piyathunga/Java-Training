caadfd1372272e693626a155b5de411f65ac59e8
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-08-31 20:58:20 +0530

    feat: Add the problem statement

---
+++ OOP3.java
@@ -0,0 +1,23 @@
+/**
+ * Write a Java program to create a class called "Rectangle" with width and height 
+ * attributes. Calculate the area and perimeter of the rectangle.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class OOP3 {
+    
+}
