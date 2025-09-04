339c62c0f29ed04e14eefa9a09cab7fb441950c7
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-09-04 08:03:51 +0530

    feat: Add the problem statement

---
+++ OOP7.java
@@ -0,0 +1,25 @@
+/**
+ * Write a Java program to create a class called "Employee" with a name, salary, and 
+ * hire date attributes, and a method to calculate years of service.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class OOP7 {
+    
+}
