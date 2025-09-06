c151df36072a78c0cdf9032bcf0be3ff5745c009
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-09-06 21:04:58 +0530

    feat: Add the problem statement

---
+++ OOP9.java
@@ -0,0 +1,19 @@
+/**
+ * Write a Java program to create a class called "Library" with a collection of 
+ * books and methods to add and remove books.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+public class OOP9 {
+    
+}
