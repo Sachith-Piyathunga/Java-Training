a480cd1f26906a79e2db7d6400dc1a23787a866c
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-09-07 15:41:57 +0530

    feat: Add the problem statement

---
+++ OOP10.java
@@ -0,0 +1,15 @@
+/**
+ * Write a Java program to create a class called "Airplane" with a flight number, 
+ * destination, and departure time attributes, and methods to check flight 
+ * status and delay.
+ */
+
+
+
+
+
+
+
+public class OOP10 {
+    
+}
