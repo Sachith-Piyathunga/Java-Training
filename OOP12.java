94de6d04fe068c4ff08c827b7da2d792d9be2ad2
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-09-09 07:57:21 +0530

    feat: Add the problem statement

---
+++ OOP12.java
@@ -0,0 +1,33 @@
+/**
+ * Write a Java program to create a class called "School" with attributes for 
+ * students, teachers, and classes, and methods to add and remove students and 
+ * teachers, and to create classes.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class OOP12 {
+    
+}
