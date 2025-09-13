be11c365093dab4056a340e1ab016be6ba015d6d
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-09-13 20:31:11 +0530

    feat: Add the probalem statement

---
+++ OOP15.java
@@ -0,0 +1,35 @@
+/**
+ * Write a Java program to create a class called "Restaurant" with attributes for menu 
+ * items, prices, and ratings, and methods to add and remove items, and to calculate 
+ * average rating.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class OOP15 {
+    
+}
