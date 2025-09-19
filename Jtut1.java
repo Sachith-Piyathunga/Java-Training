b08493238600f67fdf9de8587acd36c311296b74
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-09-19 21:24:17 +0530

    feat: Add the explanation about tha java garbage collection

---
+++ Jtut1.java
@@ -0,0 +1,53 @@
+/**
+ * Java Garbage Collection
+ * 
+ * The Java Garbage Collector (GC) is an automatic memory management system within the Java 
+ * Virtual Machine (JVM). Its primary function is to automatically reclaim memory occupied by 
+ * objects that are no longer referenced by the running Java application, thus preventing 
+ * memory leaks and simplifying memory management for developers.
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class Jtut1 {
+    
+}
