f6f2c43351b2e7e695091ec8b6b7b1ff8b66b8f1
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-09-24 22:07:22 +0530

    feat: Add the explanation for the above queary

--- DBDN1.sql
+++ DBDN1.sql
@@ -23,3 +23,22 @@ CREATE TABLE Orders_2FN (
     FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
 );
 
+
+/* 
+Explanation:
+
+Purpose of the Query :
+        - The goal is to eliminate partial dependencies by separating non-key 
+          attributes into their own tables.
+Key Components :
+        - Customers: Stores customer details independently.
+        - Orders_2NF: Links orders to customers via a foreign key.
+Why Use 2NF? :
+        - 2NF reduces redundancy by ensuring that non-key attributes depend on the 
+          entire primary key.
+        - It improves data consistency and reduces update anomalies.
+Real-World Application :
+        - In inventory systems, separating customer and order details avoids 
+          duplicating customer information.
+          
+ */
