15dc8e7a1c260beece89ffe1525f1dfcf926a149
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-09-24 19:42:01 +0530

    feat: Create an instance of CommercialBuilding

--- OOP22.java
+++ OOP22.java
@@ -156,6 +156,9 @@ public class OOP22 {
         // Create an instance of ResidentialBuilding
         ResidentialBuilding residentialBuilding = new ResidentialBuilding("99 ABC Street.", 10, 2500.0, 20, 1000.0);
         
+        // Create an instance of CommercialBuilding
+        CommercialBuilding commercialBuilding = new CommercialBuilding("100 PQR Business Avenue.", 15, 4500.0, 3000.0, 20.0);
+        
         
     }
 }
