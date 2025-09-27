6a389b31806d7351a788c111b1828e1d4c65ce53
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-09-27 21:29:11 +0530

    feat: Original table with transitive dependency.

--- DBDN2.sql
+++ DBDN2.sql
@@ -1,7 +1,12 @@
 -- Write a SQL query to eliminate transitive dependencies and achieve 3NF.
 
-
-
+-- Original table with transitive dependency.
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    DepartmentID INT,
+    DepartmentName VARCHAR(100),
+    Salary DECIMAL(10, 2)
+);
 
 
 
