692ae48d4a638e3da3de56df82ea0fb20d858be2
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-09-29 20:44:58 +0530

    feat: Add the table

--- DBDN3.sql
+++ DBDN3.sql
@@ -1,1 +1,9 @@
 -- Write a SQL query to resolve overlapping candidate keys and achieve BCNF
+
+-- Original table with overlapping candidate keys.
+CREATE TABLE Courses (
+    CourseID INT,
+    InstructorID INT,
+    InstructorName VARCHAR(100),
+    PRIMARY KEY (CourseID, InstructorID)
+);
