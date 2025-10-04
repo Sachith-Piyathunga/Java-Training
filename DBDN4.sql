382c498b0d5b08074f0b3e50b41c76ed3ba132f2
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-10-04 19:07:00 +0530

    feat: Poorly designed table with update anomalies.

--- DBDN4.sql
+++ DBDN4.sql
@@ -1,3 +1,11 @@
 /* 
 Write a SQL query to identify and resolve update anomalies in a poorly designed table.
  */
+
+-- Poorly designed table with update anomalies.
+CREATE TABLE Students (
+    StudentID INT,
+    CourseID INT,
+    CourseName VARCHAR(100),
+    InstructorName VARCHAR(100),
+);
