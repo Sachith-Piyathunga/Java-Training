cfcf9f65c077c1ad322a7b06e94daa35608ba1dd
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-10-05 20:01:39 +0530

    feat: Define th main method

--- Array1.java
+++ Array1.java
@@ -6,22 +6,10 @@
 // Import the java Array class
 import java.util.Arrays;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Define a class called Array1
 public class Array1 {
-    
+    // Define th main method
+    public static void main(String[] args) {
+        
+    }
 }
