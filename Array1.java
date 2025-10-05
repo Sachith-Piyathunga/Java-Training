faeda990807cfaf780324a8c70e5ab16744f1d25
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-10-05 20:03:47 +0530

    feat: Initialize the integer array

--- Array1.java
+++ Array1.java
@@ -10,6 +10,12 @@ import java.util.Arrays;
 public class Array1 {
     // Define th main method
     public static void main(String[] args) {
-        
+
+        // Initialize the integer array
+        int[] myArray1 = {
+           1789, 2035, 1899, 1456, 2013, 
+            1458, 2458, 1254, 1472, 2365, 
+            1456, 2165, 1457, 2456 
+        };
     }
 }
