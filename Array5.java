64d591d09a5e73c54dc4b053451952d829aa992d
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-10-06 19:35:44 +0530

    feat: Declare the integer array

--- Array5.java
+++ Array5.java
@@ -20,7 +20,11 @@ public class Array5 {
 
     // Define the main method
     public static void main(String[] args) {
-
-        
+        // Declare the integer array
+        int[] myArray = {
+            1789, 2035, 1899, 1456, 2013, 
+         1458, 2458, 1254, 1472, 2365, 
+         1456, 2265, 1457, 2456
+        };
     }
 }
