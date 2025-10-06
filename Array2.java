eb9657c06a01bdf77fb5df3cd3b0d8f845c92894
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-10-06 19:08:05 +0530

    feat: Define the main method

--- Array2.java
+++ Array2.java
@@ -1,33 +1,9 @@
 // Write a Java program to sum values of an array
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Define the Array2 class
 public class Array2 {
-    
+    // Define the main method
+    public static void main(String[] args) {
+
+    }
 }
