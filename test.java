712c7b227c6a9f16b5f7d3231781ca0fce59b6d7
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-10-06 18:49:23 +0530

    feat: Add the problem statement

---
+++ test.java
@@ -0,0 +1,18 @@
+/**
+ * Practice the question for the interview
+ * 1. How do you reverse a string in Java?
+ *      - There is no reverse() utility method in the String class. However, you can 
+ *        create a character array from the string and then iterate it from the end 
+ *        to the start. You can append the characters to a string builder and 
+ *        finally return the reversed string.
+ * 
+ */
+
+
+
+
+
+
+public class test {
+    
+}
