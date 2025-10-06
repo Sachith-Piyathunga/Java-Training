d330c9afd6b189d55aed8a238fc10cdfe9ff5124
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-10-06 19:37:18 +0530

    feat: Check if the integer array

--- Array5.java
+++ Array5.java
@@ -23,8 +23,11 @@ public class Array5 {
         // Declare the integer array
         int[] myArray = {
             1789, 2035, 1899, 1456, 2013, 
-         1458, 2458, 1254, 1472, 2365, 
-         1456, 2265, 1457, 2456
+            1458, 2458, 1254, 1472, 2365, 
+            1456, 2265, 1457, 2456
         };
+
+        // Check if the integer array 'my_array1' contains the item 2013
+        System.out.println(contains(myArray, 2013));
     }
 }
