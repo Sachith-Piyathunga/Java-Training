f44151e41097e1db19842c13b49d411b3d9b6299
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-10-06 19:32:08 +0530

    feat: Iterate through the each eliment

--- Array5.java
+++ Array5.java
@@ -5,6 +5,14 @@
 public class Array5 {
     // Define the main method
     public static void main(String[] args) {
-        
+
+        // Iterate through the each eliment
+        for (int n : arr) {
+            // Add the condition
+            if (item == n) {
+                // If a match is found
+                return true;
+            }
+        }
     }
 }
