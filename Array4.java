b293c8a32c2e26e10627785e15e87c46fd3637f4
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-10-06 19:26:04 +0530

    feat: Create the for loop

--- Array4.java
+++ Array4.java
@@ -10,5 +10,11 @@ public class Array4 {
 
         // Initialize the sum variable
         int sum = 0;
+
+        // Create the for loop
+        for (int i = 0; i < number.length; i++) {
+            // Add the current elemet of the sum
+            sum = sum + numbers[i];
+        }
     }
 }
