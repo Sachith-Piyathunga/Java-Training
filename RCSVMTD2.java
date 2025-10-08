3ce45e22e2d605086b7b78ccb975a3e948359efb
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-10-08 22:00:04 +0530

    feat: Define the main method

--- RCSVMTD2.java
+++ RCSVMTD2.java
@@ -2,7 +2,13 @@
  * Write a Java recursive method to calculate the sum of all numbers from 1 to n.
  */
 
-
+// RCSVMTD2.java
+// Main class
 public class RCSVMTD2 {
     
+
+    // Define the main method
+    public static void main(String[] args) {
+        
+    }
 }
