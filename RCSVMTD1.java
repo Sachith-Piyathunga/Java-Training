94ed58039de48bd5fc75e1ced40c4623dfddb801
Sachith-Piyathunga <sachintha.piyathunga@gmail.com>
2025-10-08 20:37:13 +0530

    feat: Define the main method

--- RCSVMTD1.java
+++ RCSVMTD1.java
@@ -2,19 +2,11 @@
  * Write a Java recursive method to calculate the factorial of a given positive integer
  */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// RCSVMTD1.java
+// Main class
 public class RCSVMTD1 {
-    
+    // Define the main method
+    public static void main(String[] args) {
+        
+    }
 }
